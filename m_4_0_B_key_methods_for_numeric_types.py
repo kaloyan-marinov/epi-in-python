ac0baef7bb69f451740e9cd1da0baa22e5f1e539
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-09-28 18:28:47 +0300

    add m_4_0_B_key_methods_for_numeric_types.py

---
+++ m_4_0_B_key_methods_for_numeric_types.py
@@ -0,0 +1,37 @@
+"""
+source for examples: EPI in Python + own examples
+"""
+
+import math
+
+print()
+print("abs(-34.5)")
+print(abs(-34.5))  # 34.5
+
+print()
+print("math.ceil(2.17)")
+print(math.ceil(2.17))  # 3
+
+print()
+print("math.floor(3.14)")
+print(math.floor(3.14))  # 3
+
+print()
+print("min(2.17, 3.14)")
+print(min(2.17, 3.14))  # 2.17
+
+print()
+print("max(2.17, 3.14)")
+print(max(2.17, 3.14))  # 3.14
+
+print()
+print("pow(7, 2)")
+print(pow(7, 2))  # 49
+
+print()
+print("7 ** 2")
+print(7 ** 2)  # 49
+
+print()
+print("math.sqrt(225)")
+print(math.sqrt(225))  # 15.0
