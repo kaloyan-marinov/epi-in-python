560af345873a7d0a2f23c29f6e75acf952c59668
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-09-28 18:33:31 +0300

    add m_4_0_C_interconvert_numeric_types_and_strings.py

---
+++ m_4_0_C_interconvert_numeric_types_and_strings.py
@@ -0,0 +1,19 @@
+"""
+source for examples: EPI in Python + own examples
+"""
+
+print()
+print("str(42)")
+print(str(42))  # 42
+
+print()
+print('int("42")')
+print(int("42"))  # 42
+
+print()
+print("str(3.14)")
+print(str(3.14))  # 3.14
+
+print()
+print('float("3.14")')
+print(float("3.14"))  # 3.14
