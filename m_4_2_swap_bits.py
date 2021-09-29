0455dd4a2d7c49670781184f84147dc50c1d0591
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-09-29 21:34:40 +0300

    implement separate functions for the two solutions within m_4_2_swap_bits.py

--- m_4_2_swap_bits.py
+++ m_4_2_swap_bits.py
@@ -10,39 +10,56 @@ def swap_bits(x: int, i: int, j: int) -> int:
     if bit_i == bit_j:
         return x
 
-    def toggle_bit(X, I):
-        bit_i = X >> I & 1
-        print("bit_i", bit_i)
+    count = 0
+    y = 0
+    while count <= j or x:
+        bit = x & 1
+        if count == i:
+            new_bit = int(not bit_i)
+        elif count == j:
+            new_bit = int(not bit_j)
+        else:
+            new_bit = bit
+        y += new_bit * (2 ** count)
+        # print(count, new_bit)
+        count += 1
+        x >>= 1
+
+    return y
+
+
+def swap_bits_2(x: int, i: int, j: int) -> int:
+    if i >= j:
+        i, j = j, i
+
+    bit_i = x >> i & 1
+    # print(bin(x))
+    bit_j = x >> j & 1
+    # print(i, bit_i)
+    # print(j, bit_j)
+    if bit_i == bit_j:
+        return x
+
+    def _toggle_bit(_x, _i):
+        bit_i = _x >> _i & 1
+        # print("bit_i", bit_i)
 
         if bit_i == 0:
-            mask_i = int("1" + I * "0", 2)
-            X = X | mask_i
+            mask_i = int("1" + _i * "0", 2)
+            _x = _x | mask_i
         else:
-            mask_remainder = int((I + 1) * "1", 2)
-            r = X - (X & mask_remainder)
-            mask_i = int("0" + I * "1", 2)
-            X = X & mask_i
-            X = X + r
-        return X
+            mask_remainder = int((_i + 1) * "1", 2)
+            r = _x - (_x & mask_remainder)
+            mask_i = int("0" + _i * "1", 2)
+            _x = _x & mask_i
+            _x = _x + r
+        return _x
 
-    print(bin(x))
-    x = toggle_bit(x, i)
-    print(bin(x))
-    x = toggle_bit(x, j)
-    print(bin(x))
-
-    # if bit_j == 1 and bit_i == 0:
-    #     mask_j = int("0" + j * "1", 2)
-    #     x = x & mask_j
-    #     mask_i = int("1" + i * "0", 2)
-    #     x = x | mask_i
-
-    # if bit_j == 0 and bit_i == 1:
-    #     mask_j = int("1" + j * "0", 2)
-    #     x = x | mask_j
-    #     mask_i = int("0" + i * "1", 2)
-    #     x = x & mask_i
-
+    # print(bin(x))
+    x = _toggle_bit(x, i)
+    # print(bin(x))
+    x = _toggle_bit(x, j)
+    # print(bin(x))
     return x
 
 
