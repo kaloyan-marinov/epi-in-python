a5645d0a725c5c765e4bdc30e82381f09a673061
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-09-29 19:07:45 +0300

    add m_4_2_swap_bits.py (which fails the EPIJudge)

---
+++ m_4_2_swap_bits.py
@@ -0,0 +1,32 @@
+def swap_bits(x: int, i: int, j: int) -> int:
+    if i >= j:
+        i, j = j, i
+
+    bit_i = x >> i & 1
+    # print(bin(x))
+    bit_j = x >> j & 1
+    # print(i, bit_i)
+    # print(j, bit_j)
+    if bit_i == bit_j:
+        return x
+
+    count = 0
+    y = 0
+    while x:
+        bit = x & 1
+        if count == i:
+            new_bit = int(not bit_i)
+        elif count == j:
+            new_bit = int(not bit_j)
+        else:
+            new_bit = bit
+        y += new_bit * (2 ** count)
+        print(count, new_bit)
+        count += 1
+        x >>= 1
+
+    return y
+
+
+result = swap_bits(int("01001001", 2), 1, 6)
+print(bin(result))
