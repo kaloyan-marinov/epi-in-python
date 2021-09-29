4e7c0811f909cacf91c2d02f43e6d6f1ba99857c
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-09-29 21:27:01 +0300

    add a 2nd solution (almost) of my own to m_4_2_swap_bits.py

--- m_4_2_swap_bits.py
+++ m_4_2_swap_bits.py
@@ -10,22 +10,40 @@ def swap_bits(x: int, i: int, j: int) -> int:
     if bit_i == bit_j:
         return x
 
-    count = 0
-    y = 0
-    while count <= j or x:
-        bit = x & 1
-        if count == i:
-            new_bit = int(not bit_i)
-        elif count == j:
-            new_bit = int(not bit_j)
-        else:
-            new_bit = bit
-        y += new_bit * (2 ** count)
-        # print(count, new_bit)
-        count += 1
-        x >>= 1
+    def toggle_bit(X, I):
+        bit_i = X >> I & 1
+        print("bit_i", bit_i)
 
-    return y
+        if bit_i == 0:
+            mask_i = int("1" + I * "0", 2)
+            X = X | mask_i
+        else:
+            mask_remainder = int((I + 1) * "1", 2)
+            r = X - (X & mask_remainder)
+            mask_i = int("0" + I * "1", 2)
+            X = X & mask_i
+            X = X + r
+        return X
+
+    print(bin(x))
+    x = toggle_bit(x, i)
+    print(bin(x))
+    x = toggle_bit(x, j)
+    print(bin(x))
+
+    # if bit_j == 1 and bit_i == 0:
+    #     mask_j = int("0" + j * "1", 2)
+    #     x = x & mask_j
+    #     mask_i = int("1" + i * "0", 2)
+    #     x = x | mask_i
+
+    # if bit_j == 0 and bit_i == 1:
+    #     mask_j = int("1" + j * "0", 2)
+    #     x = x | mask_j
+    #     mask_i = int("0" + i * "1", 2)
+    #     x = x & mask_i
+
+    return x
 
 
 result = swap_bits(int("01001001", 2), 1, 6)
