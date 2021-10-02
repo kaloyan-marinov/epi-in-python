2b5afe91149064fcce1bc657d35ffec47ca171bf
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-10-02 06:08:11 +0300

    add a 2nd solution to m_4_4_find_closest_int.py

--- m_4_4_find_closest_int.py
+++ m_4_4_find_closest_int.py
@@ -7,7 +7,7 @@ def swap_bits(x: int, i: int, j: int) -> int:
     return x
 
 
-def closest_int_same_bit_count(x: int) -> int:
+def closest_int_same_bit_count_1(x: int) -> int:
     y = None
     for i in range(63 - 1):
         mask = int((i + 2) * "1", 2)
@@ -17,8 +17,27 @@ def closest_int_same_bit_count(x: int) -> int:
     return y
 
 
+def closest_int_same_bit_count_2(x: int) -> int:
+    y = x if x & 1 == 0 else x + 1
+    mask_1 = y & ~(y - 1)  # isolates the lowest bit that is set in y
+    mask_2 = mask_1 >> 1
+    # Flip the first two consecutive bits in x that are different from each other.
+    return x ^ (mask_1 | mask_2)
+
+
 if __name__ == "__main__":
-    x = int("110000", 2)
-    print(bin(x))
-    y = closest_int_same_bit_count(x)
-    print(bin(y))
+    for binary_representation in [
+        "110000",
+        "101",
+        "1001000001101100011000111001100100101101100011",
+    ]:
+        print()
+
+        x = int(binary_representation, 2)
+        print(bin(x))
+
+        y_1 = closest_int_same_bit_count_1(x)
+        print(bin(y_1))
+
+        y_2 = closest_int_same_bit_count_2(x)
+        print(y_2 == y_1)
