ffc56229305df606e5f4e02d659d3424bd16ae32
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-10-03 11:18:35 +0300

    add a 2nd solution to m_4_6_primitive_divide.py

--- m_4_6_primitive_divide.py
+++ m_4_6_primitive_divide.py
@@ -1,4 +1,4 @@
-def quotient(x: int, y: int) -> int:
+def quotient_1(x: int, y: int) -> int:
     q = 0
     next_largest_int_multiple_of_y = y
 
@@ -13,21 +13,39 @@ def quotient(x: int, y: int) -> int:
     return q
 
 
-def is_greater_or_equal(a: int, b: int) -> bool:
+def is_greater_or_equal_1(a: int, b: int) -> bool:
     while a and b:
         a >>= 1
         b >>= 1
     return bool(a)
 
 
+def quotient_2(x: int, y: int) -> int:
+    q = 0
+    while x >= y:
+        power = max_power_of_2(x, y)
+        q += 1 << power
+        x -= y << power
+    return q
+
+
+def max_power_of_2(x: int, y: int) -> int:
+    power = 0
+    while x >= y << power:
+        power += 1
+    return power - 1
+
+
 if __name__ == "__main__":
-    q = quotient(64, 1)
-    print(is_greater_or_equal(64, 64))
-    print(is_greater_or_equal(64, 0))
-    print(is_greater_or_equal(int("1111", 2), int("1101", 2)))  # False
+    q = quotient_2(64, 1)
+
+    q = quotient_1(64, 1)
+    print(is_greater_or_equal_1(64, 64))
+    print(is_greater_or_equal_1(64, 0))
+    print(is_greater_or_equal_1(int("1111", 2), int("1101", 2)))  # False
 
     print()
-    q = quotient(64, 1)
+    q = quotient_1(64, 1)
     print(q)
-    q = quotient(64, 3)
+    q = quotient_1(64, 3)
     print(q)
