43e2468198cabc292942aaead2a894775d6e3326
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-10-04 06:49:44 +0300

    remove failed attempts from and add 1 polished solution to m_4_7_power_x_y.py

--- m_4_7_power_x_y.py
+++ m_4_7_power_x_y.py
@@ -1,4 +1,7 @@
 def solution_1_raise_to_arbitrary_power(x: int, y: int) -> int:
+    if y < 0:
+        x, y = 1 / x, -y
+
     result = 1
 
     while y:
@@ -17,52 +20,14 @@ def solution_1_raise_to_2_to_j(x: int, two_to_the_j: int) -> int:
     """
     Assume j >= 1.
     """
-    result = x
+    result = 1
+    if two_to_the_j != 1:
+        result = x
     two_to_the_j >>= 1
 
     while two_to_the_j:
         result = result * result
         two_to_the_j >>= 1
-    return result
-
-
-def failed_attempt_at_solution_2_raise_to_arbitrary_power(x, y):
-    """
-    Appears to get stuck in an infinite loop...
-    """
-    if y == 0:
-        return x
-
-    last_set_bit_of_y = y & (y - 1)
-    contrib = solution_1_raise_to_2_to_j(x, last_set_bit_of_y)
-    r = contrib
-    y = y & (~(y - 1))
-
-    while y:
-        contrib *= 2
-        if contrib == y & (y - 1):
-            r += contrib
-            y = y & (~(y - 1))
-
-    return r
-
-
-def failed_attempt_at_solution_3_raise_to_arbitrary_power(x, y):
-    """
-    Appears to return the square of the correct answer...
-    """
-
-    contrib = x
-    result = 1
-
-    while y:
-        contrib = contrib ** 2
-
-        if y & 1 == 1:
-            result *= contrib
-
-        y >>= 1
-
     return result
 
 
@@ -90,10 +55,24 @@ def solution_4_power(x: float, y: int) -> float:
     return result
 
 
+def solution_5_power(x: float, y: int) -> float:
+    if y < 0:
+        x, y = 1.0 / x, -y
+
+    result = 1.0
+    x_to_two_to_the_j = x
+
+    while y:
+        if y & 1 == 1:
+            result *= x_to_two_to_the_j
+        x_to_two_to_the_j = x_to_two_to_the_j ** 2
+        y >>= 1
+
+    return result
+
+
 if __name__ == "__main__":
     x = 1.4434757236195281
     y = 12
-    r3 = failed_attempt_at_solution_3_raise_to_arbitrary_power(x, y)
     r4 = solution_4_power(x, y)
-    print(r3)
     print(r4)
