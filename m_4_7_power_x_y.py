dc924ef03c99413fe08878910da1aca8eb351967
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-10-04 07:00:46 +0300

    fix the only broken solution in m_4_7_power_x_y.py

--- m_4_7_power_x_y.py
+++ m_4_7_power_x_y.py
@@ -18,20 +18,22 @@ def solution_1_raise_to_arbitrary_power(x: int, y: int) -> int:
 
 def solution_1_raise_to_2_to_j(x: int, two_to_the_j: int) -> int:
     """
-    Assume j >= 1.
+    Assume two_to_the_j = (1 0 ... 0)_2
     """
-    result = 1
-    if two_to_the_j != 1:
-        result = x
+    if two_to_the_j & 1 == 1:
+        return x
+
+    result = x
     two_to_the_j >>= 1
 
     while two_to_the_j:
         result = result * result
         two_to_the_j >>= 1
+
     return result
 
 
-def solution_4_power(x: float, y: int) -> float:
+def solution_2_power(x: float, y: int) -> float:
     if y < 0:
         x, y = 1 / x, -y
 
@@ -55,7 +57,7 @@ def solution_4_power(x: float, y: int) -> float:
     return result
 
 
-def solution_5_power(x: float, y: int) -> float:
+def solution_3_power(x: float, y: int) -> float:
     if y < 0:
         x, y = 1.0 / x, -y
 
@@ -72,7 +74,11 @@ def solution_5_power(x: float, y: int) -> float:
 
 
 if __name__ == "__main__":
-    x = 1.4434757236195281
+    x = -1.4434757236195281
     y = 12
-    r4 = solution_4_power(x, y)
+
+    r = solution_1_raise_to_2_to_j(x, 1)
+    print(r)
+
+    r4 = solution_2_power(x, y)
     print(r4)
