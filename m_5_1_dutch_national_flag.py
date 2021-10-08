cdca81212372acd2d4d9c5d049cd0e721fe4d77b
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-10-08 07:50:16 +0300

    fix the error encountered in the previous commit, but encounter a new error

--- m_5_1_dutch_national_flag.py
+++ m_5_1_dutch_national_flag.py
@@ -14,7 +14,14 @@ def dutch_national_flag(pivot_index: int, A: List[int]) -> List[int]:
         elif A[i] == pivot_value:
             last_ind += 1
         else:  # i.e. A[i] < pivot_value
-            A[first_ind], A[last_ind + 1], A[i] = A[i], A[first_ind], A[last_ind + 1]
+            if i == last_ind + 1:
+                A[first_ind], A[i] = A[i], A[first_ind]
+            else:
+                A[first_ind], A[last_ind + 1], A[i] = (
+                    A[i],
+                    A[first_ind],
+                    A[last_ind + 1],
+                )
             first_ind += 1
             last_ind += 1
 
@@ -24,12 +31,13 @@ def dutch_national_flag(pivot_index: int, A: List[int]) -> List[int]:
 if __name__ == "__main__":
     import copy
 
-    pivot_index = 2
-    A = [0, 1, 1, 2]
+    for pivot_index, A in (
+        (2, [0, 1, 1, 2]),
+        (0, [1, 0, 2, 0, 2, 1]),
+    ):
+        A_initial = copy.deepcopy(A)
+        A_final = dutch_national_flag(pivot_index, A)
 
-    A_initial = copy.deepcopy(A)
-    A_final = dutch_national_flag(pivot_index, A)
-
-    print()
-    print(A_initial)
-    print(A_final)
+        print()
+        print(A_initial)
+        print(A_final)
