0e15e07be0c41faf9ad3b283701d0bcbbaadad00
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-10-08 18:52:30 +0300

    add a 2nd solution to m_5_2_int_as_array_increment.py

--- m_5_2_int_as_array_increment.py
+++ m_5_2_int_as_array_increment.py
@@ -2,7 +2,7 @@ import copy
 from typing import List
 
 
-def plus_one(A: List[int]) -> List[int]:
+def solution_1_plus_one(A: List[int]) -> List[int]:
     D = copy.deepcopy(A)
     _incr_by_1(D)
     return D
@@ -18,3 +18,25 @@ def _incr_by_1(D: List[int]) -> None:
 
     if D[0] == 0:
         D.insert(0, 1)
+
+
+def solution_2_plus_one(A: List[int]) -> List[int]:
+    A[-1] += 1
+
+    for i in reversed(range(1, len(A))):
+        if A[i] != 10:
+            break
+
+        A[i] = 0
+        A[i - 1] += 1
+    else:  # the loop completed without encountering a `break` statement
+        if A[0] == 10:
+            # There is a carry-out from the input's most significant digit,
+            # which means that there is not enough storage in the array for the result,
+            # so we need 1 more digit to store the result.
+            # A slick way to do this is to append 0 to the end of the array,
+            # and update the first entry to 1.
+            A.append(0)
+            A[0] = 1
+
+    return A
