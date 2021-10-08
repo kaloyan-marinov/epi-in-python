1b8fc8a0c4c0c596e74cf23742b6477aac2f092a
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-10-08 08:17:51 +0300

    add a 2nd solution to m_5_1_dutch_national_flag.py

--- m_5_1_dutch_national_flag.py
+++ m_5_1_dutch_national_flag.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-def dutch_national_flag(pivot_index: int, A: List[int]) -> List[int]:
+def solution_1_dutch_national_flag(pivot_index: int, A: List[int]) -> List[int]:
     pivot_value = A[pivot_index]
 
     A[0], A[pivot_index] = A[pivot_index], A[0]
@@ -29,16 +29,77 @@ def dutch_national_flag(pivot_index: int, A: List[int]) -> List[int]:
     return A
 
 
+def solution_2_dutch_national_flag(pivot_index: int, A: List[int]) -> None:
+    pivot_value = A[pivot_index]
+
+    # fmt: off
+    '''
+    During partitioning
+    / During processing
+    / While constructing the [desired] partitioning,
+    maintain the following as "supporting invariants":
+        smaller          A[: first_equal]
+        equal            A[first_equal:first_unclassified]
+        unclassified     A[first_unclassified:first_larger]
+        larger           A[first_larger:]
+    '''
+    # fmt: on
+    first_equal = 0
+    first_unclassified = 0
+    first_larger = len(A)
+
+    while first_unclassified < first_larger:
+        if A[first_unclassified] < pivot_value:
+            A[first_equal], A[first_unclassified] = (
+                A[first_unclassified],
+                A[first_equal],
+            )
+            first_equal += 1
+            first_unclassified += 1
+        elif A[first_unclassified] == pivot_value:
+            first_unclassified += 1
+        else:  # i.e. A[first_unclassified] > pivot_value
+            A[first_unclassified], A[first_larger - 1] = (
+                A[first_larger - 1],
+                A[first_unclassified],
+            )
+            first_larger -= 1
+
+
 if __name__ == "__main__":
     import copy
 
-    for pivot_index, A in (
+    # Tests for solution #1.
+    inputs = (
         (2, [0, 1, 1, 2]),
         (0, [1, 0, 2, 0, 2, 1]),
-    ):
+    )
+
+    for pivot_index, A in inputs:
         A_initial = copy.deepcopy(A)
-        A_final = dutch_national_flag(pivot_index, A)
+        A_final = solution_1_dutch_national_flag(pivot_index, A)
 
         print()
-        print(A_initial)
-        print(A_final)
+        print("solution_1", A_initial)
+        print("solution_1", A_final)
+
+    # Tests for solution #2.
+    print()
+    print()
+    print()
+
+    # Note that
+    # the previous loop modified the value of `input` in-place,
+    # which means that we now have to re-initialize `intputs`.
+    inputs = (
+        (2, [0, 1, 1, 2]),
+        (0, [1, 0, 2, 0, 2, 1]),
+    )
+
+    for pivot_index, A in inputs:
+        A_initial = copy.deepcopy(A)
+        solution_2_dutch_national_flag(pivot_index, A)
+
+        print()
+        print("solution_2", A_initial)
+        print("solution_2", A)
