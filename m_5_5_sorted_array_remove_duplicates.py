469d996067712aff4b0d27364b2c3020a620ea97
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-10-12 17:39:06 +0300

    fix the failure described in the previous example, but notice that while exercising the same example by running this module works, exercising it by running the EPI Judge fails (best guess about the reason: the input array remains unmodified)

--- m_5_5_sorted_array_remove_duplicates.py
+++ m_5_5_sorted_array_remove_duplicates.py
@@ -14,8 +14,8 @@ def delete_duplicates(A: List[int]) -> int:
         if A[i] == A[i + 1]:
             A = A[:i] + A[i + 1 :]
             num_valid -= 1
-
-        i += 1
+        else:
+            i += 1
 
     print(A)
 
@@ -25,8 +25,10 @@ def delete_duplicates(A: List[int]) -> int:
 if __name__ == "__main__":
     A = [-8, -7, -6, -5, -5, -4, -3, -1, -1, 0, 0, 2, 2, 2, 4]
     A_original = copy.deepcopy(A)
-    num_valid = delete_duplicates(A)  # outputs [-8, -7, -6, -5, -4, -3, -1, 0, 2, 2, 4]
+    num_valid = delete_duplicates(A)  # outputs [-8, -7, -6, -5, -4, -3, -1, 0, 2, 4]
 
-    print(A_original)  # [-8, -7, -6, -5, -5, -4, -3, -1, -1, 0, 0, 2, 2, 2, 4]
-    # print(A)
-    print(num_valid)  # 11
+    print("A_original")
+    print(A_original)
+    print("A")
+    print(A)  # [-8, -7, -6, -5, -5, -4, -3, -1, -1, 0, 0, 2, 2, 2, 4]
+    print(num_valid)  # 10
