fde7c3c50ceefb843e9647180ce1c39e39a580cb
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-10-13 06:58:53 +0300

    add a 2nd solution to m_5_6_buy_and_sell_stock.py

--- m_5_6_buy_and_sell_stock.py
+++ m_5_6_buy_and_sell_stock.py
@@ -1,14 +1,14 @@
 from typing import List
 
 
-def buy_and_sell_stock(prices: List[float]) -> float:
+def buy_and_sell_stock_1(prices: List[float]) -> float:
     """
+    time:  O(n^2)
+    space: O(1)
+
     Test PASSED (402/402) [ 436  s]
     Average running time:    2  s
     Median running time:   246 us
-
-    time:  O(n^2)
-    space: O(1)
 
     The last 2 test cases took _very_ long to run,
     which indicates that the time complexity is _very_ high.
@@ -28,7 +28,32 @@ def buy_and_sell_stock(prices: List[float]) -> float:
     return max_profit
 
 
+def buy_and_sell_stock_2(prices: List[float]) -> float:
+    """
+    time: O(n)
+    space: O(1)
+
+    Test PASSED (402/402) [  26 ms]
+    Average running time:  187 us
+    Median running time:    27 us
+    """
+    min_over_previous_days = float("inf")
+    max_profit = 0
+
+    for price in prices:
+        min_over_previous_days = min(
+            min_over_previous_days,
+            price,
+        )
+        max_profit = max(
+            price - min_over_previous_days,
+            max_profit,
+        )
+
+    return max_profit
+
+
 if __name__ == "__main__":
     prices = [310, 315, 275, 295, 260, 270, 290, 230, 255, 250]
-    max_profit = buy_and_sell_stock(prices)
+    max_profit = buy_and_sell_stock_2(prices)
     print(max_profit)
