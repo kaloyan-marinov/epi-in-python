3828985b4b0aa6e131e42bdeca60a72e8918d5c7
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-10-14 07:45:14 +0300

    add a 2nd solution to m_5_9_enumerate_all_primes_to_n.py which fails on the provided input

--- m_5_9_enumerate_all_primes_to_n.py
+++ m_5_9_enumerate_all_primes_to_n.py
@@ -1,7 +1,16 @@
+import math
+
 from typing import List
 
 
-def generate_primes(n: int) -> List[int]:
+def generate_primes_1(n: int) -> List[int]:
+    """
+    time:  O(n^2)
+
+    Test PASSED (24/24) [3111  s]
+    Average running time:  131  s
+    Median running time:    10 us
+    """
     primes = list()
 
     if n == 1:
@@ -19,3 +28,36 @@ def generate_primes(n: int) -> List[int]:
         i += 1
 
     return primes
+
+
+def generate_primes_2(n: int) -> List[int]:
+    primes = list()
+
+    if n == 1:
+        return primes
+
+    i = 2
+    while i <= n:
+        is_prime = True
+        for j in range(
+            2,
+            math.ceil(math.sqrt(i)),
+        ):
+            if i % j == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(i)
+        i += 1
+
+    return primes
+
+
+if __name__ == "__main__":
+    n = 4
+    primes = generate_primes_2(n)
+
+    print("n")
+    print(n)
+    print("primes")
+    print(primes)  # [2, 3, 4] which is incorrect
