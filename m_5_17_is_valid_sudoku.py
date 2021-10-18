feef0802cc79d1216ce5c98ffea0f01450540696
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-10-18 08:21:45 +0300

    add an example, which demonstrates the bug mentioned in the previous commit

--- m_5_17_is_valid_sudoku.py
+++ m_5_17_is_valid_sudoku.py
@@ -42,3 +42,24 @@ def is_list_valid(numbers: Iterable[int]) -> bool:
             return False
         seen.add(n)
     return True
+
+
+if __name__ == "__main__":
+    partial_assignment = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [7, 0, 0, 5, 9, 8, 0, 2, 1],
+        [0, 1, 0, 4, 0, 0, 9, 0, 3],
+        [3, 0, 6, 7, 0, 0, 4, 0, 8],
+        [8, 2, 0, 1, 5, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 3, 0, 0, 0],
+        [0, 8, 4, 3, 0, 7, 0, 5, 0],
+        [6, 9, 0, 0, 0, 0, 2, 0, 0],
+        [1, 3, 0, 0, 0, 2, 8, 0, 7],
+    ]
+
+    is_valid = is_valid_sudoku(partial_assignment)
+
+    print("partial_assignment:")
+    print(partial_assignment)
+    print("is_valid:")
+    print(is_valid)  # False but should instead be True
