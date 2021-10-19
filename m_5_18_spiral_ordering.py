8273a379bebbdf2cab1edbb1b50a2d431a697d62
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-10-19 10:21:18 +0300

    add a refactored version of the solution in m_5_18_spiral_ordering.py

--- m_5_18_spiral_ordering.py
+++ m_5_18_spiral_ordering.py
@@ -22,3 +22,23 @@ def s_1_matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
         matrix_layer_in_clockwise(offset)
 
     return spiral_ordering
+
+
+def s_1_matrix_in_spiral_order_refactored(square_matrix: List[List[int]]) -> List[int]:
+    spiral_ordering: List[int] = []
+    for offset in range((len(square_matrix) + 1) // 2):
+        if offset == len(square_matrix) - offset - 1:
+            # `square_matrix` has odd dimension, and we are at its center.
+            spiral_ordering.append(square_matrix[offset][offset])
+            break
+
+        spiral_ordering.extend(square_matrix[offset][offset : -1 - offset])
+        spiral_ordering.extend(
+            list(zip(*square_matrix))[-1 - offset][offset : -1 - offset]
+        )
+        spiral_ordering.extend(square_matrix[-1 - offset][-1 - offset : offset : -1])
+        spiral_ordering.extend(
+            list(zip(*square_matrix))[offset][-1 - offset : offset : -1]
+        )
+
+    return spiral_ordering
