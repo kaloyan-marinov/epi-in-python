a0de2a2d2f30cd8ee3c8ae4835c6700cc9b94f7e
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-10-20 06:36:24 +0300

    fix the error mentioned in the previous commit

--- m_5_19_matrix_rotation.py
+++ m_5_19_matrix_rotation.py
@@ -2,4 +2,4 @@ from typing import List
 
 
 def rotate_matrix(square_matrix: List[List[int]]) -> None:
-    square_matrix[:] = [row[::-1] for row in zip(*square_matrix)]
+    square_matrix[:] = [list(row[::-1]) for row in zip(*square_matrix)]
