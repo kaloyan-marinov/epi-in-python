7db372da18bc29913f5fa63ec68310730f210ae7
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-11-13 15:42:54 +0100

    add m_6_05_is_string_palindromic_punctuation.py which fails on the example in the book

---
+++ m_6_05_is_string_palindromic_punctuation.py
@@ -0,0 +1,20 @@
+def is_palindrome(s: str) -> bool:
+    i = 0
+    j = len(s) - 1
+
+    while i < j:
+        if not s[i].isalnum():
+            i += 1
+            continue
+
+        if not s[j].isalnum():
+            j -= 1
+            continue
+
+        if s[i] != s[j]:
+            return False
+        else:
+            i += 1
+            j -= 1
+
+    return True
