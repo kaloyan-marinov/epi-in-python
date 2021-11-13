57a92cc5c230b1d791c9d588edf02204a73197fc
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-11-13 15:02:43 +0100

    add m_5_20_pascal_triangle.py which fails in the special case of `n=0`

---
+++ m_5_20_pascal_triangle.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+def generate_pascal_triangle(n: int) -> List[List[int]]:
+    if n == 0:
+        return [[]]
+
+    rows = [[1]]
+    if n == 1:
+        return rows
+
+    rows.append([1, 1])
+    if n == 2:
+        return rows
+
+    for new_r_idx in range(2, n):
+        prev_row = rows[new_r_idx - 1]  # rows[-1]
+        new_row = (
+            [1]
+            + [prev_row[i] + prev_row[i + 1] for i in range(len(prev_row) - 1)]
+            + [1]
+        )
+        rows.append(new_row)
+
+    return rows
