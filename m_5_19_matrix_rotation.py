e0bb9e983e3e019ae9dc1049fea30f9d2cfd38d4
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-11-13 14:45:34 +0100

    add a 2nd solution to m_5_19_matrix_rotation.py which is incorrect

--- m_5_19_matrix_rotation.py
+++ m_5_19_matrix_rotation.py
@@ -1,5 +1,34 @@
 from typing import List
 
 
-def rotate_matrix(square_matrix: List[List[int]]) -> None:
+def solution_1_rotate_matrix(square_matrix: List[List[int]]) -> None:
     square_matrix[:] = [list(row[::-1]) for row in zip(*square_matrix)]
+
+
+def solution_2_rotate_matrix(square_matrix: List[List[int]]) -> None:
+    n = len(square_matrix)
+
+    for r in range((n + 1) // 2):
+        for c in range(r, n - r):
+            (
+                square_matrix[r][c],
+                square_matrix[c][-1 - r],
+                square_matrix[-1 - r][-1 - c],
+                square_matrix[-1 - c][r],
+            ) = (
+                square_matrix[c][-1 - r],
+                square_matrix[-1 - r][-1 - c],
+                square_matrix[-1 - c][r],
+                square_matrix[r][c],
+            )
+
+
+if __name__ == "__main__":
+    A = [
+        [1, 2],
+        [3, 4],
+    ]
+
+    solution_2_rotate_matrix(A)
+
+    print(A)  # [[4, 3], [2, 1]] ( instead of the expected [[3, 1], [4, 2]] )
