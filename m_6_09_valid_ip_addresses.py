becf6b07386f6f2508b814b8a68b8a5661944dca
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-12-05 21:53:25 +0200

    add m_6_09_valid_ip_addresses.py which is incorrect

---
+++ m_6_09_valid_ip_addresses.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+def get_valid_ip_addresses(s: str) -> List[str]:
+    def _is_valid(part: str) -> bool:
+        return int(part) <= 255
+
+    valid_ips = []
+
+    for i in range(1, len(s) - 2):
+        part_1 = s[:i]
+
+        if not _is_valid(part_1):
+            continue
+
+        for j in range(i + 1, len(s) - 1):
+            part_2 = s[i:j]
+
+            if not _is_valid(part_2):
+                continue
+
+            for k in range(j + 1, len(s)):
+                part_3 = s[j:k]
+                part_4 = s[k:]
+
+                if not _is_valid(part_3) or not _is_valid(part_4):
+                    continue
+
+                valid_ips.append(".".join([part_1, part_2, part_3, part_4]))
+
+    return valid_ips
