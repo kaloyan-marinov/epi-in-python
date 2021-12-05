b8883d6cb09f08eacca2f30ef141294bcaaf2f67
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-12-05 21:08:36 +0200

    replace the previous commit's incorrect solution with the official solution

--- m_6_06_reverse_words.py
+++ m_6_06_reverse_words.py
@@ -1,4 +1,30 @@
-def reverse_words(s: str) -> str:
-    words = s.split(" ")
-    new_s = " ".join(words[::-1])
-    return new_s
+from typing import List
+
+
+def reverse_words(s: List[str]) -> None:
+    # First, reverse the whole string.
+    _reverse_slice(s, 0, len(s) - 1)
+
+    start_idx = 0
+    while True:
+        final_idx = start_idx
+        while final_idx < len(s) and s[final_idx] != " ":
+            final_idx += 1
+
+        if final_idx == len(s):
+            break
+
+        # Reverse each word in the string.
+        _reverse_slice(s, start_idx, final_idx - 1)
+
+        start_idx = final_idx + 1
+
+    # Reverse the last word.
+    _reverse_slice(s, start_idx, len(s) - 1)
+
+
+def _reverse_slice(s: List[str], start_idx: int, final_idx: int):
+    while start_idx < final_idx:
+        s[start_idx], s[final_idx] = s[final_idx], s[start_idx]
+        start_idx += 1
+        final_idx -= 1
