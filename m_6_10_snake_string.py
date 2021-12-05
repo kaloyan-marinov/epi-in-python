db8a52cb4668693ede2efbca27199872acb70519
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-12-05 22:04:55 +0200

    add a 2nd solution to m_6_10_snake_string.py

--- m_6_10_snake_string.py
+++ m_6_10_snake_string.py
@@ -1,8 +1,19 @@
-def snake_string(s: str) -> str:
+def snake_string_1(s: str) -> str:
     return s[1::4] + s[::2] + s[3::4]
+
+
+def snake_string_2(s: str) -> str:
+    result = []
+    for i in range(1, len(s), 4):
+        result.append(s[i])
+    for i in range(0, len(s), 2):
+        result.append(s[i])
+    for i in range(3, len(s), 4):
+        result.append(s[i])
+    return "".join(result)
 
 
 if __name__ == "__main__":
     s = "Hello World!"
-    r = snake_string(s)
+    r = snake_string_1(s)
     print(r)
