bf99323e5e015227181dd46c9c67584a551aa04c
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-12-05 21:42:09 +0200

    add a 2nd solution to m_6_08_roman_to_integers.py

--- m_6_08_roman_to_integers.py
+++ m_6_08_roman_to_integers.py
@@ -1,7 +1,7 @@
 import functools
 
 
-def roman_to_integer(s: str) -> int:
+def roman_to_integer_1(s: str) -> int:
     symbol_2_value = {
         "I": 1,
         "V": 5,
@@ -33,7 +33,41 @@ def roman_to_integer(s: str) -> int:
     return value
 
 
+def roman_to_integer_2(s: str) -> int:
+    """
+    Scan `s` from right to left, starting at the 2nd-to-last position:
+
+        if the symbol to the right of the current one is > than the current one:
+            subtract the current symbol
+        else:
+            add the current symbol
+    """
+
+    symbol_2_value = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+    }
+
+    value = functools.reduce(
+        lambda val, i: val
+        + (
+            -symbol_2_value[s[i]]
+            if symbol_2_value[s[i]] < symbol_2_value[s[i + 1]]
+            else symbol_2_value[s[i]]
+        ),
+        reversed(range(len(s) - 1)),
+        symbol_2_value[s[-1]],
+    )
+
+    return value
+
+
 if __name__ == "__main__":
     s = "II"
-    v = roman_to_integer(s)
+    v = roman_to_integer_1(s)
     print(v)
