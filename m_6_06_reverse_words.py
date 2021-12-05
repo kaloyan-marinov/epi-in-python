d5f710e626dba517ccb3cae986cd40b3d86db97a
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-12-05 21:15:49 +0200

    refactor the solution in a less mysterious way

--- m_6_06_reverse_words.py
+++ m_6_06_reverse_words.py
@@ -5,25 +5,32 @@ def reverse_words(s: List[str]) -> None:
     # First, reverse the whole string.
     _reverse_slice(s, 0, len(s) - 1)
 
-    start_idx = 0
-    while True:
-        final_idx = start_idx
-        while final_idx < len(s) and s[final_idx] != " ":
-            final_idx += 1
+    # Second, reverse each individual word (that is present within the string).
+    word_start = 0
+    while word_start < len(s):
+        # Determine the start and final indices that "frame" the current word.
+        if s[word_start] == " ":
+            word_start += 1
+            continue
 
-        if final_idx == len(s):
-            break
+        word_final = word_start
+        while s[word_final] != " ":
+            word_final += 1
+            if word_final == len(s):
+                break
 
-        # Reverse each word in the string.
-        _reverse_slice(s, start_idx, final_idx - 1)
+        # Reverse the current word.
+        _reverse_slice(s, word_start, word_final - 1)
 
-        start_idx = final_idx + 1
-
-    # Reverse the last word.
-    _reverse_slice(s, start_idx, len(s) - 1)
+        # Initialize the start for the next word.
+        word_start = word_final + 1
 
 
 def _reverse_slice(s: List[str], start_idx: int, final_idx: int):
+    """
+    Perform an in-place change of `s`, reversing `s[start_idx : final_idx + 1]`.
+    (Recall that the last entry in `s[start_idx : final_idx + 1]` is `s[final_idx]`.)
+    """
     while start_idx < final_idx:
         s[start_idx], s[final_idx] = s[final_idx], s[start_idx]
         start_idx += 1
