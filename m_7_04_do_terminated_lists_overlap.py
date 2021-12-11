1ebf1e046138bd608bc9c0fba88f740ea00cfa4c
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-12-11 21:16:57 +0200

    make the implementation easier to understand

--- m_7_04_do_terminated_lists_overlap.py
+++ m_7_04_do_terminated_lists_overlap.py
@@ -1,12 +1,13 @@
 from typing import Optional
 
-from m_7_00_common import ListNode
+from m_7_00_common import ListNode, length
 
 
 def overlapping_no_cycle_lists(l0: ListNode, l1: ListNode) -> Optional[ListNode]:
     if l0 is None or l1 is None:
         return None
 
+    # Check if the two lists terminate with the same node.
     t0 = l0
     while t0.next:
         t0 = t0.next
@@ -16,24 +17,20 @@ def overlapping_no_cycle_lists(l0: ListNode, l1: ListNode) -> Optional[ListNode]
         t1 = t1.next
 
     if t0 is not t1:
+        # The 2 lists don't overlap.
         return None
 
-    # The 2 lists are now known to overlap.
-    length_0 = 0
-    i0 = l0
-    while i0:
-        length_0 += 1
-        i0 = i0.next
+    # The 2 lists are now known to overlap
+    # - go on to find the 1st overlapping node.
+    length_0 = length(l0)
+    length_1 = length(l1)
 
-    length_1 = 0
-    i1 = l1
-    while i1:
-        length_1 += 1
-        i1 = i1.next
-
+    # To simplify the code, ensure that `l1` is the longer linked list.
     if length_0 > length_1:
         l0, l1 = l1, l0
         length_0, length_1 = length_1, length_0
+
+    # Advance `l1` by the length difference.
     i = length_1 - length_0
     while i > 0:
         l1 = l1.next
