4c9c81c6a8c37f51b3c07cccbd39a11c46c3b572
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-12-11 20:37:23 +0200

    fix the bug in the previous commit

--- m_7_03_is_list_cyclic.py
+++ m_7_03_is_list_cyclic.py
@@ -4,25 +4,12 @@ from m_7_00_common import ListNode
 
 
 def has_cycle(head: ListNode) -> Optional[ListNode]:
-    """
-    Running this through EPIJudge crashes on the following test: 1/102.
-    The given reason for the crash is
-        ```
-        exception message: unhashable type: 'ListNode'
-        ```
-
-    The problematic test in question is included below,
-    namely in this module's `if __name__ == "__main__"` block.
-    Curiously,
-    using this repo's virtual environment to run that same test does not crash
-    but returns the correct answer.
-    """
     seen_nodes = set()
     while head:
-        if head in seen_nodes:
+        if id(head) in seen_nodes:
             return head
         else:
-            seen_nodes.add(head)
+            seen_nodes.add(id(head))
             head = head.next
 
 
