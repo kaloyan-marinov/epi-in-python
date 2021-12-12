de3ef316c1cfdfb6f7839f2803ee6e48e85dc519
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-12-12 11:04:06 +0100

    add a 2nd, more performant solution to m_7_11_is_list_palindromic.py

--- m_7_11_is_list_palindromic.py
+++ m_7_11_is_list_palindromic.py
@@ -1,7 +1,7 @@
-from m_7_00_common import ListNode, length
+from m_7_00_common import ListNode, length, reverse_list
 
 
-def is_linked_list_a_palindrome(L: ListNode) -> bool:
+def is_linked_list_a_palindrome_1(L: ListNode) -> bool:
     """
     strategy/approach:
         - compare the 1st and last nodes
@@ -35,10 +35,48 @@ def is_linked_list_a_palindrome(L: ListNode) -> bool:
     return True
 
 
+def is_linked_list_a_palindrome_2(L: ListNode) -> bool:
+    """
+    strategy/approach:
+        - pay a 1-time cost of O(n) time complexity
+          to obtain the reverse of the 2nd half of the original list
+        - test for palindromicity
+          by checking whether the 1st half and the reversed 2nd half are equal
+
+    NB: this strategy/approach does change the original list,
+        but the original list can be restored by reversing the already-reversed sublist
+        (as soon as the palindromicity test has been performed)
+
+    time:  O(n)
+    space: O(1)
+    """
+
+    # Find the second half of L.
+    slow = L
+    fast = L
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    # Reverse the second half.
+    reversed_second_half_it = reverse_list(slow)
+
+    # Compare the 1st half and the reversed 2nd half.
+    first_half_it = L
+    while first_half_it and reversed_second_half_it:
+        if first_half_it.data != reversed_second_half_it.data:
+            return False
+
+        first_half_it = first_half_it.next
+        reversed_second_half_it = reversed_second_half_it.next
+
+    return True
+
+
 if __name__ == "__main__":
     # Test: 1/203
     L = None
-    r = is_linked_list_a_palindrome(L)
+    r = is_linked_list_a_palindrome_2(L)
     print(r)  # True (as expected)
 
     # Test: 3/203
@@ -47,5 +85,5 @@ if __name__ == "__main__":
         [2, 5, 3, 1, 3, 5, 3, 4, 2, 5, 5, 6, 3, 2, 2, 4, 3, 4, 5, 6, 6, 5, 4, 3, 4, 2, 2, 3, 6, 5, 5, 2, 4, 3, 5, 3, 1, 3, 5, 2]
     )
     # fmt: on
-    r = is_linked_list_a_palindrome(L)
+    r = is_linked_list_a_palindrome_2(L)
     print(r)  # True (as expected)
