068fd2285aac5493a868f6baa2e98f84e9c1f597
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-12-12 10:24:06 +0100

    add a 2nd solution to m_7_10_even_odd_list_merge.py

--- m_7_10_even_odd_list_merge.py
+++ m_7_10_even_odd_list_merge.py
@@ -3,7 +3,11 @@ from typing import Optional
 from m_7_00_common import ListNode
 
 
-def even_odd_merge(L: Optional[ListNode]) -> Optional[ListNode]:
+def even_odd_merge_1(L: Optional[ListNode]) -> Optional[ListNode]:
+    """
+    Consider the nodes of (the linked list) L to be indexed starting at 0.
+    """
+
     if L is None:
         return
 
@@ -28,3 +32,35 @@ def even_odd_merge(L: Optional[ListNode]) -> Optional[ListNode]:
         odd = odd.next
 
     return L
+
+
+def even_odd_merge_2(L: Optional[ListNode]) -> Optional[ListNode]:
+    """
+    Consider the nodes of (the linked list) L to be indexed starting at 0.
+    """
+
+    if L is None:
+        return
+
+    # Allocate 2 new nodes,
+    # which will serve as dummy heads of 2 initially single-node lists.
+    even_dummy_head = ListNode(data=0, next=L)
+    odd_dummy_head = ListNode(data=0, next=L)
+
+    # Re-use the nodes of the input list
+    # by iterating through the list
+    #    and appending even elements to the 1st "new list"
+    #              and odd elements to the 2nd "new list".
+    tails = [even_dummy_head, odd_dummy_head]
+    turn = 0  # represents a 0/1 variable to indicate which list to append to.
+    while L:
+        tails[turn].next = L
+        L = L.next
+        tails[turn] = tails[turn].next
+        turn ^= 1  # achieves alternation between even and odd.
+
+    # Append "the odd list" to "the even one".
+    tails[1].next = None
+    tails[0].next = odd_dummy_head.next
+
+    return even_dummy_head.next
