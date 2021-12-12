5b35ecaf3c2ee543fa0c32607c6a6861a479739b
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-12-12 06:54:14 +0200

    add clarifying comments

--- m_7_06_delete_node_from_list.py
+++ m_7_06_delete_node_from_list.py
@@ -5,10 +5,10 @@ def deletion_from_list(node_to_delete: ListNode) -> None:
     """
     Assume `node_to_delete` is not the tail node.
     """
-    n = node_to_delete
+    n = node_to_delete  # actually, can use `node_to_delete` directly instead of `n`
 
     n.data = n.next.data
 
     n_next_next = n.next.next
-    n.next.next = None
+    n.next.next = None  # actually, this can be commented out, b/c `n.next` represents the deleted node
     n.next = n_next_next
