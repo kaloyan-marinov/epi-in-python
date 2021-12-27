ea50f6705ee2e1eb8f4ea49c6ba0274c897079b2
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-12-27 21:10:01 +0200

    attempt to fix the bug in the previous commmit, but end up making things worse

--- m_8_05_sunset_view.py
+++ m_8_05_sunset_view.py
@@ -8,13 +8,13 @@ def examine_buildings_with_sunset(sequence: Iterator[int]) -> List[int]:
         if heights and heights[-1] <= s_i:
             while heights and heights[-1] <= s_i:
                 heights.pop()
-            heights.append(s_i)
+            heights.append(i)
         else:
-            heights.append(s_i)
+            heights.append(i)
     return heights
 
 
 if __name__ == "__main__":
     sequence = [6, 3, 4]
     heights = examine_buildings_with_sunset(sequence)
-    print(heights)  # `[6, 4]` (but `[2, 0]` is expected)
+    print(heights)  # `[2]` (but `[2, 0]` is expected)
