f2b38980ab7b4a9ac3751f3305efe3b7e1359b7d
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-12-27 22:33:49 +0200

    rename variables + add comments

--- m_8_09_queue_with_max.py
+++ m_8_09_queue_with_max.py
@@ -4,25 +4,38 @@ import collections
 
 
 class QueueWithMax:
+    """
+    Consider a queue element `s`,
+    which entered the queue before a later element `x` with `x > s`.
+    Since `s` will be dequeued before `x`,
+    `s` can never in the future become the queue's max
+    (regardless of any subsequent enqueues and/or dequeues).
+
+    The idea is to eliminate elements like the above-described `s`
+    from consideration [for the queue's max]
+    - we can do that by maintaining a separate deque of those queue elements,
+    for each of which there doesn't exist a "later and greater" entity.
+    """
+
     def __init__(self):
         self._entries: Deque[Any] = collections.deque()
-        self._candidates: Deque[Any] = collections.deque()
+        self._candidates_for_max: Deque[Any] = collections.deque()
 
     def enqueue(self, x: int) -> None:  # Harder than `dequeue`!
         self._entries.append(x)
 
-        while self._candidates and self._candidates[-1] < x:
-            self._candidates.pop()
+        while self._candidates_for_max and x > self._candidates_for_max[-1]:
+            self._candidates_for_max.pop()
 
-        self._candidates.append(x)
+        self._candidates_for_max.append(x)
 
     def dequeue(self) -> int:
         v = self._entries.popleft()
 
-        if self._candidates[0] == v:
-            self._candidates.popleft()
+        if self._candidates_for_max[0] == v:
+            self._candidates_for_max.popleft()
 
         return v
 
     def max(self):
-        return self._candidates[0]
+        return self._candidates_for_max[0]
