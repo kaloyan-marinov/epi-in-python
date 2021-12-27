98517e3e36c5d8533f06943498daccdba32aa58d
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-12-27 15:17:24 +0200

    add a 2nd solution with better time complexity (at the cost of worsening the [overall] space complexity to O(n))

--- m_8_01_stack_with_max.py
+++ m_8_01_stack_with_max.py
@@ -1,4 +1,8 @@
-class Stack:
+import collections
+from typing import List
+
+
+class StackBruteForce:
     def __init__(self):
         self.values = []
 
@@ -12,4 +16,38 @@ class Stack:
         return self.values.pop()
 
     def max(self) -> int:
+        """
+        time: O(n)
+        """
         return max(self.values)
+
+
+ElementWithCachedMax = collections.namedtuple(
+    "ElementWithCachedMax",
+    ("element", "max"),
+)
+
+
+class Stack:
+    def __init__(self) -> None:
+        self._element_max_pairs: List[ElementWithCachedMax] = []
+
+    def empty(self) -> bool:
+        return len(self._element_max_pairs) == 0
+
+    def push(self, x: int) -> None:
+        self._element_max_pairs.append(
+            ElementWithCachedMax(
+                x,
+                x if self.empty() else max(x, self.max()),
+            )
+        )
+
+    def pop(self) -> int:
+        return self._element_max_pairs.pop().element
+
+    def max(self) -> int:
+        """
+        time: O(1)
+        """
+        return self._element_max_pairs[-1].max
