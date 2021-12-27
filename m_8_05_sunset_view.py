605eeb23a0144c52f5ae9b8e2d97b771612d5906
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-12-27 21:18:21 +0200

    get m_8_05_sunset_view.py to work (by introducing `HeightIndexPair` and keeping track of a list of such instances)

--- m_8_05_sunset_view.py
+++ m_8_05_sunset_view.py
@@ -2,16 +2,28 @@ from collections import namedtuple
 from typing import Iterator, List
 
 
+HeightIndexPair = namedtuple(
+    "HeightIndexPair",
+    ("height", "idx"),
+)
+
+
 def examine_buildings_with_sunset(sequence: Iterator[int]) -> List[int]:
-    heights = []
+    """
+    `sequence`:     represents buildings in east-to-west order,
+                    with each building specified by its height
+    """
+    height_index_pairs: List[HeightIndexPair] = []
+
     for i, s_i in enumerate(sequence):
-        if heights and heights[-1] <= s_i:
-            while heights and heights[-1] <= s_i:
-                heights.pop()
-            heights.append(s_i)
+        if height_index_pairs and height_index_pairs[-1].height <= s_i:
+            while height_index_pairs and height_index_pairs[-1].height <= s_i:
+                height_index_pairs.pop()
+            height_index_pairs.append(HeightIndexPair(s_i, i))
         else:
-            heights.append(s_i)
-    return heights
+            height_index_pairs.append(HeightIndexPair(s_i, i))
+
+    return [h_i_p.idx for h_i_p in reversed(height_index_pairs)]
 
 
 if __name__ == "__main__":
