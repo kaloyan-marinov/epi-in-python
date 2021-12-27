0202b72b3b909e44f124b081fd86a870b7ddc043
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-12-27 21:10:47 +0200

    `git checkout HEAD~1 -- m_8_05_sunset_view.py`

--- m_8_05_sunset_view.py
+++ m_8_05_sunset_view.py
@@ -8,13 +8,13 @@ def examine_buildings_with_sunset(sequence: Iterator[int]) -> List[int]:
         if heights and heights[-1] <= s_i:
             while heights and heights[-1] <= s_i:
                 heights.pop()
-            heights.append(i)
+            heights.append(s_i)
         else:
-            heights.append(i)
+            heights.append(s_i)
     return heights
 
 
 if __name__ == "__main__":
     sequence = [6, 3, 4]
     heights = examine_buildings_with_sunset(sequence)
-    print(heights)  # `[2]` (but `[2, 0]` is expected)
+    print(heights)  # `[6, 4]` (but `[2, 0]` is expected)
