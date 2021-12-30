da080d875a94eb93cb5552218cfbd81daafd96da
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-12-30 20:29:27 +0200

    demonstrate explicitly how sampling a size-`k` subset of `{0, ..., n - 1}` can be viewed/treated as a special case of sampling a size-`k` subset of a size-`n` array `A`

--- m_5_15_random_subset.py
+++ m_5_15_random_subset.py
@@ -2,6 +2,10 @@ from typing import List
 
 import random
 
+from m_5_12_offline_sampling import (
+    random_sampling as generate_random_sample_in_inplace_manner,
+)
+
 
 def random_subset(n: int, k: int) -> List[int]:
     """
@@ -17,8 +21,14 @@ def random_subset(n: int, k: int) -> List[int]:
     """
     A = list(range(n))
 
+    # The next statement is identical to the following commented-out block of code.
+    generate_random_sample_in_inplace_manner(k, A)
+    # fmt: off
+    '''
     for i in range(k):
         r = random.randrange(i, len(A))
         A[i], A[r] = A[r], A[i]
+    '''
+    # fmt: on
 
     return A[:k]
