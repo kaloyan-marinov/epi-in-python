e28a8aa6b679be552f3b2ec6487595631a780242
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-12-30 06:21:15 +0200

    remove 1 incorrect solution from m_5_12_offline_sampling.py + write a docstring for the remaining (correct) solution

--- m_5_12_offline_sampling.py
+++ m_5_12_offline_sampling.py
@@ -2,26 +2,22 @@ import random
 from typing import List
 
 
-def random_sampling_1(k: int, A: List[int]) -> None:
+def random_sampling(k: int, A: List[int]) -> None:
     """
-    Test PASSED (8/8) [ 419 ms]
-    Average running time:  368 ms
-    Median running time:   335 ms
-    """
-    _A = list()
-    for i in range(k):
-        _a = random.choice(A)
-        while _a in _A:
-            _a = random.choice(A)
-        _A.append(_a)
-    A[:] = _A
+    Assume that `A` is an array of distinct elements.
 
+    Generate - in a uniformly random manner! - a sample of size `k` from `A`.
+    (
+    Return - at random! - a subset of size `k` from `A`,
+    in such a way that
+    each subset of size `k` is equally likely to be returned.
+    )
 
-def random_sampling_2(k: int, A: List[int]) -> None:
-    """
-    Test PASSED (8/8) [ 312 ms]
-    Average running time:  313 ms
-    Median running time:   308 ms
+    The [random] sample is generated
+    by modifying `A` in-place in such a way that
+      (a) _the elements of_ the generated random sample are in the `A[:k]` subarray, and
+      (b) the remaining elements are in the last `n - k` slots.
+    In other words, the generated [random] sample is `set(A[:k])`.
     """
     for i in range(k):
         r = random.randrange(i, len(A))  # equivalent to random.randint(i, len(A) - 1)
