37214a9402691d59bcd444bc2ce362f67f8631cf
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2021-12-31 18:38:05 +0200

    add 3rd solution to m_5_16_generate_nonuniform_random_numbers.py (which is handy for demonstration purposes, but is actually unacceptable)

--- m_5_16_generate_nonuniform_random_numbers.py
+++ m_5_16_generate_nonuniform_random_numbers.py
@@ -5,13 +5,39 @@ import random
 from typing import List
 
 
-def solution_1_nonuniform_random_number(
+def nonuniform_random_number_1(
     values: List[int],
     probabilities: List[float],
 ) -> int:
     """
-    time:  <= O(n) where n := len(values)
-    space: O(1)    beyond the inputs
+    time:  need to check the documentation of Python's Standard Library
+
+    space: need to check the documentation of Python's Standard Library
+
+    ---
+
+    While this implementation works,
+    it does not demonstrate the knowledge/understanding
+    that the problem is designed to test.
+    """
+    choices = random.choices(
+        values,
+        weights=probabilities,
+        k=1,
+    )
+    return choices[0]
+
+
+def nonuniform_random_number_2(
+    values: List[int],
+    probabilities: List[float],
+) -> int:
+    """
+    time:  <= O(n)
+           where n := len(values)
+
+    space: O(1)
+           beyond the inputs
 
     Test PASSED (10/10) [ 462 ms]
     Average running time:  534 ms
@@ -29,7 +55,7 @@ def solution_1_nonuniform_random_number(
     return values[idx]
 
 
-def solution_2_nonuniform_random_number(
+def nonuniform_random_number_3(
     values: List[int],
     probabilities: List[float],
 ) -> int:
@@ -40,19 +66,20 @@ def solution_2_nonuniform_random_number(
            once the array is constructed,
            O(log n)
 
-    space: O(n) [which is the space required by the array of intervals]
+    space: O(n)
+           [which is the space required by the array of intervals]
 
     Test PASSED (10/10) [   2  s]
     Average running time:    1  s
     Median running time:     1  s
     """
 
-    prefix_sum_of_probabilities = list(
+    right_endpoints_for_subintervals = list(
         itertools.accumulate(probabilities),
     )
 
     interval_idx = bisect.bisect(
-        prefix_sum_of_probabilities,
+        right_endpoints_for_subintervals,
         random.random(),
     )
 
@@ -65,15 +92,15 @@ if __name__ == "__main__":
     # fmt: off
     '''
     probabilities = [0, 0.5, 0.3, 0.1, 0.1]
-    prefix_sum_of_probabilities = list(
+    right_endpoints_for_subintervals = list(
         itertools.accumulate(probabilities),
     )
-    print(prefix_sum_of_probabilities)  # [0.0, 0.5, 0.8, 0.9, 1]
+    print(right_endpoints_for_subintervals)  # [0.0, 0.5, 0.8, 0.9, 1]
     '''
     # fmt: on
 
     probabilities = [0.05, 0.1, 0.25, 0.2, 0.4]
-    prefix_sum_of_probabilities = list(
+    right_endpoints_for_subintervals = list(
         itertools.accumulate(probabilities),
     )
-    print(prefix_sum_of_probabilities)  # [0.05, 0.15, 0.4, 0.6, 1.0]
+    print(right_endpoints_for_subintervals)  # [0.05, 0.15, 0.4, 0.6, 1.0]
