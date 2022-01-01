7701c0f22b506463cf4f35d22e80ccc9cbc74dbe
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-01-01 15:12:28 +0200

    extract part of m_5_0_C_key_operations_with_lists.py into a new module called m_5_0_D_bisection_algorithm_aka_binary_search.py

--- m_5_0_C_key_operations_with_lists.py
+++ m_5_0_C_key_operations_with_lists.py
@@ -47,59 +47,6 @@ print(zip_A_B)  # [(0, 0), (2, 3), (4, 9)]
 
 # fmt: off
 '''
-binary search for a sorted list
-'''
-# fmt: on
-import bisect
-
-x = 4
-
-# fmt: off
-'''
-Locate the insertion point for x in A to maintain sorted order.
-
-If x is already present in A,
-the insertion point will be before (to the left of) any existing entries.
-
-    The returned insertion point i partitions the array A into two halves so that
-        all(val < x for val in A[lo : i])
-        for the left side
-    and
-        all(val >= x for val in A[i : hi])
-        for the right side.
-
-The return value is suitable for use as the first parameter to `A.insert()`
-assuming that A is already sorted.
-'''
-# fmt: on
-
-print()
-print("bisect.bisect_left(A, x)")
-print(bisect.bisect_left(A, x))  # 2
-
-# fmt: off
-'''
-Similar to bisect_left(),
-but returns an insertion point
-which comes after (to the right of) any existing entries of x in a.
-
-    The returned insertion point i partitions the array A into two halves so that
-        all(val <= x for val in A[lo : i]) for the left side
-    and
-        all(val > x for val in A[i : hi]) for the right side.
-'''
-# fmt: on
-print()
-print("bisect.bisect_right(A, x)")
-print(bisect.bisect_right(A, x))  # 4
-
-print()
-print("bisect.bisect(A, x)")
-print(bisect.bisect(A, x))  # 4
-
-
-# fmt: off
-'''
 Reverse a list.
 '''
 # fmt: on
