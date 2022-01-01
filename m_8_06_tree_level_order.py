d206fafb699a1ece11245411c525e3a2388fc9b6
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-01-01 14:45:13 +0200

    refactor the previous commit's function (into what I consider a clearer implementation)

--- m_8_06_tree_level_order.py
+++ m_8_06_tree_level_order.py
@@ -13,11 +13,12 @@ def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
     while curr_depth_nodes:
         result.append([c_d_n.data for c_d_n in curr_depth_nodes])
 
-        curr_depth_nodes = [
-            child
-            for c_d_n in curr_depth_nodes
-            for child in (c_d_n.left, c_d_n.right)
-            if child
-        ]
+        next_depth_nodes: List[BinaryTreeNode] = []
+        for c_d_n in curr_depth_nodes:
+            for child in (c_d_n.left, c_d_n.right):
+                if child:
+                    next_depth_nodes.append(child)
+
+        curr_depth_nodes = next_depth_nodes
 
     return result
