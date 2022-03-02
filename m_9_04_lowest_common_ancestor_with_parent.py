fb57ea08f0ddb93d17da4448b280e5bbfabb52f2
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-02 08:37:21 +0200

    add m_9_04_lowest_common_ancestor_with_parent.py

---
+++ m_9_04_lowest_common_ancestor_with_parent.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+from m_9_00_common import BinaryTreeNode
+
+
+def lca(
+    node0: BinaryTreeNode,
+    node1: BinaryTreeNode,
+) -> Optional[BinaryTreeNode]:
+    def _depth(n: BinaryTreeNode) -> int:
+        d = 0
+        while n.parent:
+            d += 1
+            n = n.parent
+        return d
+
+    d0 = _depth(node0)
+    d1 = _depth(node1)
+
+    if d0 > d1:
+        node0, node1 = node1, node0
+        d0, d1 = d1, d0
+
+    for _ in range(d1 - d0):
+        node1 = node1.parent
+
+    while node0 != node1:
+        node0 = node0.parent
+        node1 = node1.parent
+
+    return node0
