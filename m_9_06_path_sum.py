404490c79a8c144fe8f06fecafef011cb66fe2d2
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-02 08:42:00 +0200

    add m_9_06_path_sum.py

---
+++ m_9_06_path_sum.py
@@ -0,0 +1,21 @@
+from m_9_00_common import BinaryTreeNode
+
+
+def has_path_sum(tree: BinaryTreeNode, remaining_weight: int) -> bool:
+    found = False
+
+    def _helper(t, x: int) -> bool:
+        nonlocal found
+
+        if found:
+            return True
+
+        x += t.data
+
+        if not t.left and not t.right:
+            found = x == remaining_weight
+
+        _helper(t.left, x)
+        _helper(t.right, x)
+
+    return _helper(tree, 0)
