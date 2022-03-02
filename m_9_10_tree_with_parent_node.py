3271b8082614d86b58de3b9e69c7f77d737497bc
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-02 08:43:27 +0200

    add m_9_10_tree_with_parent_node.py

---
+++ m_9_10_tree_with_parent_node.py
@@ -0,0 +1,32 @@
+from typing import List, Optional
+
+from m_9_00_common import BinaryTreeNode
+
+
+def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
+    result: List[int] = []
+    prev_n: Optional[BinaryTreeNode] = None
+
+    while tree:
+        if prev_n is tree.parent:  # i.e. we came down to `tree` from `prev_n`
+            if tree.left:
+                next_n = tree.left
+            else:
+                result.append(tree.data)
+                # Ready to move on from the current `tree`, so:
+                # - if right is not empty, go right;
+                # - else, go up.
+                next_n = tree.right or tree.parent
+        elif prev_n is tree.left:  # i.e. we came up to `tree` from its left child
+            result.append(tree.data)
+            # Ready to move on from the current `tree`, so:
+            # - if right is not empty, go right;
+            # - else, go up.
+            next_n = tree.right or tree.parent
+        else:  # i.e. done with both children
+            next_n = tree.parent
+
+        prev_n = tree
+        tree = next_n
+
+    return result
