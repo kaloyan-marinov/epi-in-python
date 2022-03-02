d2760d9e4f82a8c478f7c577f9b34c4834d381ea
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-02 08:44:45 +0200

    add m_9_13_tree_connect_leaves.py

---
+++ m_9_13_tree_connect_leaves.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from m_9_00_common import BinaryTreeNode
+
+
+def create_list_of_leaves(
+    tree: BinaryTreeNode,
+) -> List[BinaryTreeNode]:
+
+    leaves: List[BinaryTreeNode] = []
+
+    def _helper(t: BinaryTreeNode, ls: List[BinaryTreeNode]):
+        if t:
+            _helper(t.left, ls)
+
+            if not t.left and not t.right:
+                ls.append(t)
+
+            _helper(t.right, ls)
+
+    _helper(tree, leaves)
+
+    return leaves
