f1416acad7955c0ca64bc6fcb3cd3fd9cd63b0d2
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-10 21:21:03 +0200

    clean up m_14_04_lower_common_ancestor_in_bst.py

--- m_14_04_lower_common_ancestor_in_bst.py
+++ m_14_04_lower_common_ancestor_in_bst.py
@@ -5,17 +5,22 @@ from m_14_00_common import BstNode
 
 def find_lca_1(tree: BstNode, s: BstNode, b: BstNode) -> Optional[BstNode]:
     """
-    Assume that all the nodes in `tree` have distinct keys.
+    Assume that:
+        - all the nodes in `tree` have distinct keys
+        - the input nodes are nonempty
+        - the input nodes are part of the `tree`
     """
 
-    # Ensure that `s.data <= b.data`.
+    # Ensure that `s.data < b.data`.
+    # (The following ensures that,
+    # since the keys have already been assumed to be pairwise distinct.)
     if s.data > b.data:
         s, b = b, s
 
-    while tree and (b.data <= tree.data or tree.data <= s.data):
-        if b.data <= tree.data:
+    while tree and (b.data < tree.data or tree.data < s.data):
+        if b.data < tree.data:
             tree = tree.left
-        elif tree.data <= s.data:
+        elif tree.data < s.data:
             tree = tree.right
 
     return tree
@@ -23,20 +28,21 @@ def find_lca_1(tree: BstNode, s: BstNode, b: BstNode) -> Optional[BstNode]:
 
 def find_lca_2(tree: BstNode, s: BstNode, b: BstNode) -> Optional[BstNode]:
     """
-    Assume that all the nodes in `tree` have distinct keys.
-
-    Input nodes are nonempty
-    and `s.data <= b.data`.
+    Assume that:
+        - all the nodes in `tree` have distinct keys
+        - the input nodes are nonempty
+        - the input nodes are part of the `tree`
+        - `s.data < b.data` (since the keys have already been assumed to be pairwise distinct)
     """
 
     while tree.data < s.data or b.data < tree.data:
         # Keep searching since `tree` is outside [s, b].
 
-        while tree.data < s.data:
+        while tree.data < s.data:  # this can be converted into an `if` statement
             # LCA must be in `tree`'s right subtree.
             tree = tree.right
 
-        while b.data < tree.data:
+        while b.data < tree.data:  # this can be converted into an `if` statement
             # LCA must be in `tree`'s left subtree.
             tree = tree.left
 
