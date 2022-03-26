43b6b5870dc1cca91f80357707f6fb65c82adde7
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-26 11:51:44 +0200

    fix m_17_08_largest_rectangle_under_skyline.py

--- m_17_08_largest_rectangle_under_skyline.py
+++ m_17_08_largest_rectangle_under_skyline.py
@@ -1,31 +1,51 @@
 from typing import List
 
 
-def calculate_largest_rectangle(heights: List[int]) -> int:
+def calculate_largest_rectangle_1(heights: List[int]) -> int:
     """
-    This is _very_ incomplete and consequently also incorrect.
+    This function is based on the idea of
+    determining the widest rectangle that is "supported" by building `i`,
+    for each `i`.
+
+    In other words, the idea is,
+    for each building `i`,
+    to find the widest rectangle that building `i` acts as a "pillar" for.
     """
-    start_indices: List[int] = [None] * len(heights)
-    start_indices[0] = 0
 
-    max_area = float("-inf")
+    max_rectangle_area = 0
 
-    for i, h_i in enumerate(heights[1:]):
-        # if heights[i - 1] <= h_i:
-        if heights[i - 1] > h_i:
-            start_indices[i] = start_indices[i - 1]
-        else:
-            start_indices[i] = i
+    for i, h_i in enumerate(heights):
+        left = i
+        while left - 1 >= 0 and heights[left - 1] >= h_i:
+            left = left - 1
 
-        max_area = max(
-            max_area,
-            (i - start_indices[i] + 1) * h_i,
+        right = i
+        while right + 1 <= len(heights) - 1 and heights[right + 1] >= h_i:
+            right = right + 1
+
+        max_rectangle_area = max(
+            max_rectangle_area,
+            (right - left + 1) * h_i,
         )
 
-    return max_area
+    return max_rectangle_area
 
 
-def calculate_largest_rectangle(heights: List[int]) -> int:
+def calculate_largest_rectangle_2(heights: List[int]) -> int:
+    """
+    This function is based on the same idea as the former one.
+
+    However, this function's implementation
+    is a modification of the former function's implementation,
+    which (modification) sacrifices space for time.
+    """
+
+    # As we advance through the buildings:
+    # (a) all we really need to keep track of is
+    #     the buildings that have not been blocked yet, and
+    # (b) we can "replace" existing buildings,
+    #     whose heights equal the height of the current building,
+    #     "with the current building"
     pillar_indices: List[int] = []
 
     max_rectangle_area = 0
@@ -37,7 +57,6 @@ def calculate_largest_rectangle(heights: List[int]) -> int:
         while pillar_indices and heights[pillar_indices[-1]] >= h_i:
             height = heights[pillar_indices.pop()]
 
-            # width = i if not pillar_indices else i - pillar_indices[-1] - 1
             if pillar_indices:
                 width = i - (pillar_indices[-1] + 1)
             else:
@@ -52,4 +71,4 @@ def calculate_largest_rectangle(heights: List[int]) -> int:
 
 if __name__ == "__main__":
     heights = [1, 4, 2, 5, 6, 3, 2, 6, 6, 5, 2, 1, 3]
-    result = calculate_largest_rectangle(heights)
+    result = calculate_largest_rectangle_2(heights)
