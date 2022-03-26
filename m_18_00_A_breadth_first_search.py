ada0766e41fc376d0f7b092b0bdf452474133d7e
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-26 16:42:17 +0200

    add `bfs_v_2` to m_18_00_breadth_first_search.py

--- m_18_00_A_breadth_first_search.py
+++ m_18_00_A_breadth_first_search.py
@@ -1,10 +1,13 @@
-from typing import Dict, List
+from typing import Dict, List, Deque
+
+
+import collections
 
 
 Vertex = str
 
 
-def bfs(
+def bfs_v_1(
     Adj: Dict[Vertex, List[Vertex]],
     s: Vertex,
 ) -> Dict[Vertex, List[Vertex]]:
@@ -33,5 +36,36 @@ def bfs(
 
         frontier = next_frontier
         i += 1
+
+    return vertex_2_level
+
+
+def bfs_v_2(
+    Adj: Dict[Vertex, List[Vertex]],
+    s: Vertex,
+) -> Dict[Vertex, List[Vertex]]:
+    """
+    space: O(|V| + |E|)
+    time:  O(|V| + |E|)
+    """
+
+    vertex_2_level: Dict[Vertex, int] = {s: 0}
+    # (The following is optional;
+    # just gives us another potentially helpful structure.)
+    vertex_2_parent: Dict[Vertex, Vertex] = {s: None}
+
+    q: Deque[Vertex] = collections.deque(
+        [s],
+    )
+
+    while q:
+        u = q.popleft()
+
+        for v in Adj[u]:
+            if v not in vertex_2_level:
+                vertex_2_level[v] = vertex_2_level[u] + 1
+                vertex_2_parent[v] = u
+
+                q.append(v)
 
     return vertex_2_level
--- test_18_00_A_breadth_first_search.py
+++ test_18_00_A_breadth_first_search.py
@@ -2,14 +2,14 @@ import unittest
 
 from typing import Dict, List
 
-from m_18_00_A_breadth_first_search import bfs
+from m_18_00_A_breadth_first_search import bfs_v_1, bfs_v_2
 
 
 class TestBfs(unittest.TestCase):
-    def test_bfs_output(self):
-        Vertex = str
+    def setUp(self):
+        self.Vertex = str
 
-        adj: Dict[Vertex, List[Vertex]] = {
+        self.adj: Dict[self.Vertex, List[self.Vertex],] = {
             "a": ["s", "z"],
             "z": ["a"],
             "s": ["a", "x"],
@@ -20,16 +20,27 @@ class TestBfs(unittest.TestCase):
             "v": ["c", "f"],
         }
 
-        v_2_lvl: Dict[Vertex, int] = bfs(adj, "s")
-
         # fmt: off
+        self.expected_vertex_2_level = {
+            's': 0,
+            'a': 1, 'x': 1,
+            'z': 2, 'c': 2, 'd': 2,
+            'f': 3, 'v': 3,
+        }
+        # fmt: on
+
+    def test_bfs_v_1(self):
+        v_2_lvl: Dict[self.Vertex, int] = bfs_v_1(self.adj, "s")
+
         self.assertEqual(
             v_2_lvl,
-            {
-                's': 0,
-                'a': 1, 'x': 1,
-                'z': 2, 'c': 2, 'd': 2,
-                'f': 3, 'v': 3,
-            },
+            self.expected_vertex_2_level,
         )
-        # fmt: on
+
+    def test_bfs_v_2(self):
+        v_2_lvl: Dict[self.Vertex, int] = bfs_v_2(self.adj, "s")
+
+        self.assertEqual(
+            v_2_lvl,
+            self.expected_vertex_2_level,
+        )
