0d362f56595f1ebb7e8bf68b94af20910d7e4498
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-26 12:45:32 +0200

    "fix" m_04_11_rectangle_intersection.py

--- m_04_11_rectangle_intersection.py
+++ m_04_11_rectangle_intersection.py
@@ -2,61 +2,13 @@ import collections
 from typing import Tuple, Optional
 
 
+Rect = collections.namedtuple("Rect", ("x", "y", "width", "height"))
+
+
 Point2D = collections.namedtuple(
     "Point2D",
     ["x", "y"],
 )
-
-
-# fmt: off
-def _rectangles_intersection(
-    r_1_llc: Point2D, r_1_urc: Point2D,
-    r_2_llc: Point2D, r_2_urc: Point2D,
-) -> Optional[Tuple[Point2D, Point2D]]:
-    """
-    Determine the intersection of the rectangles r_1 and r_2,
-    where each rectangle is specified by its lower-left corner and upper-right corner
-    (i.e. by its "llc" and "urc").
-    """
-    intersection_x = _intervals_intersection(
-        r_1_llc.x, r_1_urc.x,
-        r_2_llc.x, r_2_urc.x,
-    )
-    intersection_y = _intervals_intersection(
-        r_1_llc.y, r_1_urc.y,
-        r_2_llc.y, r_2_urc.y,
-    )
-
-    if not intersection_x or not intersection_y:
-        return None
-    else:
-        '''
-        return (
-            intersection_x[0],
-            intersection_y[0],
-            intersection_x[1],
-            intersection_y[1]
-        )
-        '''
-        llc = Point2D(intersection_x[0], intersection_y[0])
-        urc = Point2D(intersection_x[1], intersection_y[1])
-        return llc, urc
-# fmt: on
-
-
-def _intervals_intersection(a_1: float, b_1: float, a_2: float, b_2: float):
-    """
-    Determine the intersection of the intervals [a_1, b_1] and [a_2, b_2].
-    """
-    a = max(a_1, a_2)
-    b = min(b_1, b_2)
-    if a > b:
-        return None
-    else:
-        return a, b
-
-
-Rect = collections.namedtuple("Rect", ("x", "y", "width", "height"))
 
 
 def intersect_rectangle(r1: Rect, r2: Rect) -> Rect:
@@ -79,8 +31,50 @@ def intersect_rectangle(r1: Rect, r2: Rect) -> Rect:
         return Rect(x, y, width, height)
 
 
-# def intersect_rectangle_wrapper(r1, r2):
-#     return intersect_rectangle(Rect(*r1), Rect(*r2))
+def _rectangles_intersection(
+    r_1_llc: Point2D,
+    r_1_urc: Point2D,
+    r_2_llc: Point2D,
+    r_2_urc: Point2D,
+) -> Optional[Tuple[Point2D, Point2D]]:
+    """
+    Determine the intersection of the rectangles r_1 and r_2,
+    where each rectangle is specified by its lower-left corner and upper-right corner
+    (i.e. by its "llc" and "urc").
+    """
+    intersection_x = _intervals_intersection(
+        r_1_llc.x,
+        r_1_urc.x,
+        r_2_llc.x,
+        r_2_urc.x,
+    )
+    intersection_y = _intervals_intersection(
+        r_1_llc.y,
+        r_1_urc.y,
+        r_2_llc.y,
+        r_2_urc.y,
+    )
+
+    if not intersection_x or not intersection_y:
+        return None
+    else:
+        llc = Point2D(intersection_x[0], intersection_y[0])
+        urc = Point2D(intersection_x[1], intersection_y[1])
+        return llc, urc
+
+
+def _intervals_intersection(
+    a_1: float, b_1: float, a_2: float, b_2: float
+) -> Optional[Tuple[float, float]]:
+    """
+    Determine the intersection of the intervals [a_1, b_1] and [a_2, b_2].
+    """
+    a = max(a_1, a_2)
+    b = min(b_1, b_2)
+    if a > b:
+        return None
+    else:
+        return a, b
 
 
 if __name__ == "__main__":
@@ -102,8 +96,8 @@ if __name__ == "__main__":
     # fmt: off
     '''
     Arguments
-	r1:       [54, 66, 66, 24]
-	r2:       [27, 97, 68, 95]
+    r1:       [54, 66, 66, 24]
+    r2:       [27, 97, 68, 95]
 
     Failure info
         expected: [0, 0, -1, -1]
