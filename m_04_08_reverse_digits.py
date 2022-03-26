269485df3f086ec8fadc6e35d197967d6d89674b
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-26 12:36:49 +0200

    add 1 more solution to and clean up m_04_08_reverse_digits.py

--- m_04_08_reverse_digits.py
+++ m_04_08_reverse_digits.py
@@ -1,6 +1,16 @@
-def reverse_digits(x: int) -> int:
+def reverse(x: int) -> int:
+    """
+    Return the integer corresponding to
+    the digits of `x`['s decimal representation] written in reverse order.
+
+    For example, the reverse of 42 is 24, and the reverse of -314 is -413.
+
+    time:  O(n)
+           where n := the # of digits in the decimal representation of `x`
+    """
+
     if x < 0:
-        return -reverse_digits(-x)
+        return -reverse(-x)
 
     y = 0
     while x:
@@ -8,3 +18,23 @@ def reverse_digits(x: int) -> int:
         x //= 10
 
     return y
+
+
+def reverse_2(x: int) -> int:
+    """
+    Return the integer corresponding to
+    the digits of `x`['s decimal representation] written in reverse order.
+
+    For example, the reverse of 42 is 24, and the reverse of -314 is -413.
+
+    time:  O(n)
+           where n := the # of digits in the decimal representation of `x`
+    """
+
+    result, abs_x = 0, abs(x)
+
+    while abs_x:
+        result = result * 10 + abs_x % 10
+        abs_x //= 10
+
+    return result if x >= 0 else -result
