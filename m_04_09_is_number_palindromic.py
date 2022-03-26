fbefa5ae2967d6241f3efa0e73585c159aac1ba8
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-26 12:39:41 +0200

    add 1 more solution to and clean up m_04_09_is_number_palindromic.py

--- m_04_09_is_number_palindromic.py
+++ m_04_09_is_number_palindromic.py
@@ -1,24 +1,26 @@
-def solution_1_is_palindrome(x: int) -> bool:
+from m_04_08_reverse_digits import reverse
+
+
+def solution_1_is_palindrome_number(x: int) -> bool:
+    """
+    time:  O(n)
+           where n := the # of digits in the decimal representation of `x`
+    """
+
     if x < 0:
         return False
-    return x == solution_1__reverse_digits(x)
+
+    return x == reverse(x)
 
 
-def solution_1__reverse_digits(x: int) -> int:
-    if x < 0:
-        return -solution_1__reverse_digits(abs(x))
-
-    y = 0
-    while x:
-        y = y * 10 + (x % 10)
-        x //= 10
-
-    return y
+import math
 
 
-def solution_2_is_palindrome(x: int) -> bool:
+def solution_2_is_palindrome_number(x: int) -> bool:
     if x < 0:
         return False
+    elif x == 0:
+        return True
 
     max_index = solution_2__least_power_of_10(x) - 1
     k = 0
@@ -34,17 +36,48 @@ def solution_2_is_palindrome(x: int) -> bool:
 
 
 def solution_2__least_power_of_10(x: int) -> int:
+    # fmt: off
+    '''
     power = 0
     while x >= 10 ** power:
         power += 1
     return power
+    '''
+    # fmt: on
+
+    # The commented-out block above works fine, but is slower than:
+    return math.floor(math.log10(x)) + 1
 
 
 def solution_2__extract_digit_at_index(x: int, k: int) -> int:
     return (x // (10 ** k)) % 10
 
 
+def solution_3_is_palindrome_number(x: int) -> bool:
+    if x < 0:
+        return False
+    elif x == 0:
+        return True
+
+    num_digits = math.floor(math.log10(x)) + 1
+    msd_mask = 10 ** (num_digits - 1)
+
+    for i in range(num_digits // 2):
+        if x // msd_mask != x % 10:
+            return False
+
+        # Remove the most significant digit (MSD) of `x`.
+        x %= msd_mask
+
+        # Remove the least significant digit (MLD) of `x`.
+        x //= 10
+
+        msd_mask //= 100
+
+    return True
+
+
 if __name__ == "__main__":
     x = 9
-    is_x_palindrome = solution_2_is_palindrome(x)
+    is_x_palindrome = solution_2_is_palindrome_number(x)
     print(x, is_x_palindrome)
