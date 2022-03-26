1bfa8e09a8b402abe67cb84aa2cf438e58d675b2
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-26 12:20:17 +0200

    clean up m_04_03_reverse_bits.py

--- m_04_03_reverse_bits.py
+++ m_04_03_reverse_bits.py
@@ -1,34 +1,57 @@
-def swap_bits(x: int, i: int, j: int) -> int:
-    bit_i = x >> i & 1
-    bit_j = x >> j & 1
-    if bit_i != bit_j:
-        # Construct a bitmask for "selecting"/"pinpointing" the bits
-        # at the positions/indices of interest.
-        bitmask = (1 << i) | (1 << j)
-        # Utilize the constructed bitmask to flip each of those bits.
-        x ^= bitmask
+from typing import List, Optional
+
+
+from m_04_02_swap_bits import swap_bits
+
+
+def reverse_bits_1(x: int) -> int:
+    """
+    This is suitable
+    if the operation needs to be performed only once.
+
+    Assume that `x` is a 64-bit integer word.
+
+    time:  O(n)
+           where
+           n := the # of bits in `x`,
+    """
+    for i in range(32):
+        x = swap_bits(x, i, 63 - i)
     return x
 
 
-d = dict()
+def reverse_bits_2(x: int) -> int:
+    """
+    This leverages a cache
+    if the operation needs to be performed repeatedly.
+
+    Assume that `x` is a 64-bit integer word.
+
+    time:  O(n / L)
+           where
+           n := the # of bits in `x`,
+           L := the # of bits in the cache's keys
+    """
+
+    bitmask = 0xFFFF
+
+    x_0 = x & bitmask
+    x_1 = (x >> 16) & bitmask
+    x_2 = (x >> 32) & bitmask
+    x_3 = (x >> 48) & bitmask
+
+    return (
+        WORD_OF_16_BITS_2_ITS_REVERSE[x_3]
+        | (WORD_OF_16_BITS_2_ITS_REVERSE[x_2] << 16)
+        | (WORD_OF_16_BITS_2_ITS_REVERSE[x_1] << 32)
+        | (WORD_OF_16_BITS_2_ITS_REVERSE[x_0] << 48)
+    )
+
+
+WORD_OF_16_BITS_2_ITS_REVERSE: List[Optional[int]] = [None for _ in range(2 ** 16)]
 
 for x in range(2 ** 16):
     y = x
     for i in range(8):
         y = swap_bits(y, i, 15 - i)
-    d[x] = y
-
-
-def reverse_bits_1(x: int) -> int:
-    bitmask = 0xFFFF
-    x_0 = x & bitmask
-    x_1 = (x >> 16) & bitmask
-    x_2 = (x >> 32) & bitmask
-    x_3 = (x >> 48) & bitmask
-    return d[x_3] | (d[x_2] << 16) | (d[x_1] << 32) | (d[x_0] << 48)
-
-
-def reverse_bits_2(x: int) -> int:
-    for i in range(32):
-        x = swap_bits(x, i, 63 - i)
-    return x
+    WORD_OF_16_BITS_2_ITS_REVERSE[x] = y
