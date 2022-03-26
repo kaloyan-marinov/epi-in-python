6bce469e7848e87ece2b673446fdf0ca303139a5
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-26 13:51:28 +0200

    clean up m_05_05_sorted_array_remove_dups.py

--- m_05_05_sorted_array_remove_dups.py
+++ m_05_05_sorted_array_remove_dups.py
@@ -5,53 +5,19 @@ from typing import List
 
 def delete_duplicates_1(A: List[int]) -> int:
     """
-    Test PASSED (2003/2003) [  51  s]
-    Average running time:   26 ms
-    Median running time:    20 us
-    """
-    num_valid = len(A)
-
-    i = 0
-    while i < num_valid:
-        if i + 1 == num_valid:
-            break
-
-        if A[i] == A[i + 1]:
-            A[i:] = A[i + 1 :]
-            num_valid -= 1
-        else:
-            i += 1
-
-    return num_valid
-
-
-def delete_duplicates_2(A: List[int]) -> int:
-    """
-    Test PASSED (2003/2003) [  32 ms]
-    Average running time:   96 us
-    Median running time:    29 us
-    """
-    i = 0
-
-    while i < len(A):
-        num_copies_of_a_i = 1  # error!
-        while (
-            i + num_copies_of_a_i < len(A) and A[i] == A[i + num_copies_of_a_i]
-        ):  # error!
-            num_copies_of_a_i += 1
-
-        if num_copies_of_a_i >= 2:
-            A[i + 1 :] = A[i + num_copies_of_a_i :]  # error!
-
-        i += 1
-
-    return len(A)
-
-
-def delete_duplicates_3(A: List[int]) -> int:
-    """
     This solution is based on brute force.
-    time: O(n)
+
+    Assume that `A` is a sorted array.
+
+    Perform an in-place modification of `A` s.t.
+    (a) all duplicate values get removed from `A`, and
+    (b) the remaining values get shifted to the left to fill the emptied cells.
+    (There are no requirements as to the values stored beyond the last valid cell.)
+
+    Return the # of valid cells.
+
+    time:  O(n)
+
     space: O(n)
     """
     unique_values = set()
@@ -67,12 +33,79 @@ def delete_duplicates_3(A: List[int]) -> int:
     return len(A)
 
 
+def delete_duplicates_2(A: List[int]) -> int:
+    """
+    Assume that `A` is a sorted array.
+
+    Perform an in-place modification of `A` s.t.
+    (a) all duplicate values get removed from `A`, and
+    (b) the remaining values get shifted to the left to fill the emptied cells.
+    (There are no requirements as to the values stored beyond the last valid cell.)
+
+    Return the # of valid cells.
+
+    time:  O(n)
+
+    space: O(1)
+    """
+
+    if not A:
+        return 0  # NB: `return 17` also works!
+
+    # Observe that this function's return value is guaranteed to be >= 1.
+    count_unique_values = 1
+
+    i = 1
+    while i < len(A):
+        latest_unique_value = A[count_unique_values - 1]
+
+        if A[i] != latest_unique_value:
+            A[count_unique_values] = A[i]
+            count_unique_values += 1
+
+        i += 1
+
+    return count_unique_values
+
+
+def delete_duplicates_3(A: List[int]) -> int:
+    """
+    Assume that `A` is a sorted array.
+
+    Perform an in-place modification of `A` s.t.
+    (a) all duplicate values get removed from `A`, and
+    (b) the remaining values get shifted to the left to fill the emptied cells.
+    (There are no requirements as to the values stored beyond the last valid cell.)
+
+    Return the # of valid cells.
+
+    time:  O(n)
+
+    space: O(1)
+    """
+
+    if not A:
+        return 0  # NB: `return 17` also works!
+
+    # Observe that this function's return value is guaranteed to be >= 1.
+    count_unique_values = 1
+
+    for i in range(1, len(A)):
+        latest_unique_value = A[count_unique_values - 1]
+
+        if A[i] != latest_unique_value:
+            A[count_unique_values] = A[i]
+            count_unique_values += 1
+
+    return count_unique_values
+
+
 if __name__ == "__main__":
     # fmt: off
     A = [-8, -7, -6, -5, -5, -4, -3, -1, -1, 0, 0, 2, 2, 2, 4]
     # fmt: on
     A_original = copy.deepcopy(A)
-    num_valid = delete_duplicates_3(A)  # outputs [-8, -7, -6, -5, -4, -3, -1, 0, 2, 4]
+    num_valid = delete_duplicates_2(A)  # outputs [-8, -7, -6, -5, -4, -3, -1, 0, 2, 4]
 
     print("A_original")
     print(A_original)
