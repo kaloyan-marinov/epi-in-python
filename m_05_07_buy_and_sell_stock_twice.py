c9dbc0af7117764be395577aeb97eed6ac6754f1
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-26 13:55:36 +0200

    clean up m_05_07_buy_and_sell_stock_twice.py

--- m_05_07_buy_and_sell_stock_twice.py
+++ m_05_07_buy_and_sell_stock_twice.py
@@ -1,18 +1,19 @@
 from typing import List
 
-from m_5_6_buy_and_sell_stock import buy_and_sell_stock_2 as _buy_and_sell_stock_once
+from m_05_06_buy_and_sell_stock import buy_and_sell_stock_2 as _buy_and_sell_stock_once
 
 
 def buy_and_sell_stock_twice_1(prices: List[float]) -> float:
     """
-    time:  O()
-    space: O(1)
-    Test PASSED (402/402) [ 588  s]
-    Average running time:    2  s
-    Median running time:   533 us
+    (The last 2 of the provided test cases take a _very_ long time to run.)
 
-    The last 2 test cases took _very_ long to run,
-    which indicates that the time complexity is _very_ high.
+    The `prices` represent the daily prices of a particular stock.
+
+    Return the max profit that can be made
+    by buying and selling 1 share of the stock in question at most twice.
+
+    time:  O(n^2)
+    space: O(1)
     """
     max_profit = _buy_and_sell_stock_once(prices)
 
@@ -40,36 +41,55 @@ def buy_and_sell_stock_twice_1(prices: List[float]) -> float:
 
 def buy_and_sell_stock_twice_2(prices: List[float]) -> float:
     """
+    (The last 2 of the provided test cases take a _very_ long time to run.)
+
+    The `prices` represent the daily prices of a particular stock.
+
+    Return the max profit that can be made
+    by buying and selling 1 share of the stock in question at most twice.
+
     time:  O(n)
     space: O(n)
-
-    Test PASSED (402/402) [  67 ms]
-    Average running time:  486 us
-    Median running time:    81 us
     """
+
+    # Forward pass: (pre-)compute a cache,
+    # whose `i`-th cell stores the max profit if
+    # (a) we are allowed to make a single sale, and
+    # (b) do make the sale on day `i`.
+    day_idx_2_max_profit_for_single_sale_on_or_before_day_idx: List[float] = [
+        0.0
+    ] * len(prices)
+
     max_total_profit = 0.0
-
-    # Forward phase:
-    # record max profit if we make the 1st sell on that day.
-    profits_for_1st_sell_on_i_th_day = [0.0] * len(prices)
-
     min_price_over_prev_days = float("inf")
-    for i, p_i in enumerate(prices):
-        profit_from_first_sell_on_day_i = p_i - min_price_over_prev_days
-        max_total_profit = max(max_total_profit, profit_from_first_sell_on_day_i)
-        min_price_over_prev_days = min(min_price_over_prev_days, p_i)
-        profits_for_1st_sell_on_i_th_day[i] = max_total_profit
 
-    # Backward phase:
-    # find max profit if we make the 2nd buy on that day.
-    max_price_over_next_days = float("-inf")
-    for i in reversed(range(1, len(prices))):
-        p_i = prices[i]
-        profit_from_2nd_buy_on_day_i = max_price_over_next_days - p_i
-        max_price_over_next_days = max(max_price_over_next_days, p_i)
+    for i, p_i in enumerate(prices):
+        min_price_over_prev_days = min(
+            min_price_over_prev_days,
+            p_i,
+        )
+        max_total_profit = max(
+            p_i - min_price_over_prev_days,
+            max_total_profit,
+        )
+        day_idx_2_max_profit_for_single_sale_on_or_before_day_idx[i] = max_total_profit
+
+    # Backward pass: iteratively determine the max profit if
+    # we make the 2nd buy on day `j`.
+    max_total_profit = 0.0
+    max_price_over_day_j_and_later = float("-inf")
+
+    for j in reversed(range(1, len(prices))):
+        p_j = prices[j]
+        max_price_over_day_j_and_later = max(max_price_over_day_j_and_later, p_j)
+
+        profit_from_1st_buy_sell = (
+            day_idx_2_max_profit_for_single_sale_on_or_before_day_idx[j]
+        )
+        profit_from_2nd_buy_sell = max_price_over_day_j_and_later - p_j
         max_total_profit = max(
             max_total_profit,
-            profits_for_1st_sell_on_i_th_day[i] + profit_from_2nd_buy_on_day_i,
+            profit_from_1st_buy_sell + profit_from_2nd_buy_sell,
         )
 
     return max_total_profit
