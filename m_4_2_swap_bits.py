f1d9ddf1d96330d615198f4dd17b26f737cd5da1
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-26 12:15:37 +0200

    clean up m_4_2_swap_bits.py

--- m_4_2_swap_bits.py
+++ m_4_2_swap_bits.py
@@ -3,10 +3,7 @@ def swap_bits_1(x: int, i: int, j: int) -> int:
         i, j = j, i
 
     bit_i = x >> i & 1
-    # print(bin(x))
     bit_j = x >> j & 1
-    # print(i, bit_i)
-    # print(j, bit_j)
     if bit_i == bit_j:
         return x
 
@@ -21,7 +18,6 @@ def swap_bits_1(x: int, i: int, j: int) -> int:
         else:
             new_bit = bit
         y += new_bit * (2 ** count)
-        # print(count, new_bit)
         count += 1
         x >>= 1
 
@@ -30,16 +26,12 @@ def swap_bits_1(x: int, i: int, j: int) -> int:
 
 def swap_bits_2(x: int, i: int, j: int) -> int:
     bit_i = x >> i & 1
-    # print(bin(x))
     bit_j = x >> j & 1
-    # print(i, bit_i)
-    # print(j, bit_j)
     if bit_i == bit_j:
         return x
 
     def _toggle_bit(_x, _i):
         bit_i = _x >> _i & 1
-        # print("bit_i", bit_i)
 
         if bit_i == 0:
             mask_i = int("1" + _i * "0", 2)
@@ -52,11 +44,8 @@ def swap_bits_2(x: int, i: int, j: int) -> int:
             _x = _x + r
         return _x
 
-    # print(bin(x))
     x = _toggle_bit(x, i)
-    # print(bin(x))
     x = _toggle_bit(x, j)
-    # print(bin(x))
     return x
 
 
