7e773ed801ed485fe35fb2002792a3aaba9aaf02
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-26 12:01:14 +0200

    clean up m_18_03_matrix_enclosed_regions.py

--- m_18_03_matrix_enclosed_regions.py
+++ m_18_03_matrix_enclosed_regions.py
@@ -1,13 +1,13 @@
 import collections
-from typing import Deque, List, Set, Tuple
+from typing import Deque, List, Set, Tuple, Literal
 
 
 def fill_surrounded_regions(
-    board: List[List[str]],  # change to `List[List[Literal['W', 'B']]]`
+    board: List[List[Literal["W", "B"]]],
 ) -> None:
     """
     (Assuming that `board` is rectangle-shaped,)
-    Replace each 'W' that cannot reach the boundary with a 'B'.
+    Replace each 'W', which cannot reach the boundary, with a 'B'.
     """
 
     n_rows = len(board)
@@ -15,16 +15,6 @@ def fill_surrounded_regions(
 
     white_cells_that_can_reach_the_boundary: Set[Tuple[int, int]] = set()
 
-    # fmt: off
-    '''
-    q: Deque[Tuple[int, int]] = collections.deque(
-        board[0]
-        + [board[row][0] for row in range(1, n_rows - 1)]
-        + [board[row][n_cols - 1] for row in range(1, n_rows - 1)]
-        + board[n_rows - 1]
-    )
-    '''
-    # fmt: on
     q: Deque[Tuple[int, int]] = collections.deque(
         [(0, col) for col in range(n_cols)]
         + [(row, 0) for row in range(1, n_rows - 1)]
@@ -36,11 +26,6 @@ def fill_surrounded_regions(
         x, y = q.popleft()
 
         if board[x][y] == "W":
-            # fmt: off
-            '''
-            white_cells_that_can_reach_the_boundary.append((x, y))
-            '''
-            # fmt: on
             white_cells_that_can_reach_the_boundary.add((x, y))
 
             for next_x, next_y in (
@@ -49,15 +34,6 @@ def fill_surrounded_regions(
                 (x, y + 1),
                 (x, y - 1),
             ):
-                # fmt: off
-                '''
-                if (
-                    0 <= next_x < n_rows
-                    and 0 <= next_y < n_cols
-                    and board[next_x][next_y] == "W"
-                ):
-                '''
-                # fmt: on
                 if (
                     0 <= next_x < n_rows
                     and 0 <= next_y < n_cols
@@ -66,48 +42,38 @@ def fill_surrounded_regions(
                 ):
                     q.append((next_x, next_y))
 
-        # print(len(q))
-
     for row in range(n_rows):
         for col in range(n_cols):
-            # fmt: off
-            '''
-            if (row, col) in white_cells_that_can_reach_the_boundary:
-            '''
-            # fmt: on
             if (row, col) not in white_cells_that_can_reach_the_boundary:
                 board[row][col] = "B"
 
 
 def fill_surrounded_regions_v2(
-    board: List[List[str]],  # change to `List[List[Literal['W', 'B']]]`
+    board: List[List[Literal["W", "B"]]],
 ) -> None:
     """
     (Assuming that `board` is rectangle-shaped,)
-    Replace each 'W' that cannot reach the boundary with a 'B'.
+    Replace each 'W', which cannot reach the boundary, with a 'B'.
     """
     n_rows = len(board)
     n_cols = len(board[0])
 
-    # fmt: off
-    q = collections.deque(
-        [
-            (i, j)
-            for k in range(n_rows)
-            for i, j in ((k, 0), (k, n_cols - 1))
-        ] + [
-            (i, j)
-            for k in range(n_cols)
-            for i, j in ((0, k), (n_rows - 1, k))
-        ]
+    q: Deque[Tuple[int, int]] = collections.deque(
+        [(0, col) for col in range(n_cols)]
+        + [(row, 0) for row in range(1, n_rows - 1)]
+        + [(row, n_cols - 1) for row in range(1, n_rows - 1)]
+        + [(n_rows - 1, col) for col in range(n_cols)]
     )
-    # fmt: on
 
     while q:
         x, y = q.popleft()
 
         if 0 <= x < n_rows and 0 <= y < n_cols and board[x][y] == "W":
+            # The following statement,
+            # while inconsistent with the type annotation of this function's input,
+            # represents a clever way of introducing and utilizing a Temporary color.
             board[x][y] = "T"
+
             q.extend(
                 [
                     (x - 1, y),
@@ -119,7 +85,7 @@ def fill_surrounded_regions_v2(
 
     # fmt: off
     board[:] = [
-        ["B" if c != "T" else "W" for c in row]
+        ["W" if c == "T" else "B" for c in row]
         for row in board
     ]
     # fmt: on
