4828ac85f082d8a765ff2487ee55530a0e867b31
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-26 10:33:53 +0200

    fix m_15_07_enumerate_balanced_parentheses.py

--- m_15_07_enumerate_balanced_parentheses.py
+++ m_15_07_enumerate_balanced_parentheses.py
@@ -3,53 +3,18 @@ from typing import List
 
 def generate_balanced_parentheses(num_pairs: int) -> List[str]:
     """
-    Test FAILED ( 4/11) [  21 us]
+    For example:
+
+    Test ... ( 4/11) ...
     Arguments
         num_pairs: 3
 
-    Failure info
+    ...
         expected:  ['((()))', '(()())', '(())()', '()(())', '()()()']
-        result:    ['((()))', '(()())', '()(())', '()()()']
+        ...
     """
-    # fmt: off
-    '''
-    result: List[str] = []
 
-    def _helper(n_pairs) -> List[str]:
-        if n_pairs == 0:
-            return [""]
-        if n_pairs == 1:
-            return ["()"]
-
-        temp = _helper(n_pairs - 1)
-        return ["(" + p + ")" for p in temp] + ["()" + p for p in temp]
-
-    return _helper(num_pairs)
-    '''
-    # fmt: on
-    result: List[str] = []
-
-    def _helper(n_pairs) -> List[str]:
-        if n_pairs == 2:
-            return ["(())"]
-
-        temp = _helper(n_pairs - 1)
-        return (
-            ["(" + p + ")" for p in temp]
-            + ["()" + p for p in temp[:-1]]
-            + [p + "()" for p in temp[:-1]]
-        )
-
-    if num_pairs == 0:
-        return [""]
-    elif num_pairs == 1:
-        return ["()"]
-
-    return _helper(num_pairs) + ["()" * num_pairs]
-
-
-def generate_balanced_parentheses_2(num_pairs: int) -> List[str]:
-    def _directed_generate_balanced_parentheses(
+    def _guided_generate_balanced_parentheses(
         num_left_parens_needed: int,
         num_right_parens_needed: int,
         valid_prefix: str,
@@ -57,14 +22,14 @@ def generate_balanced_parentheses_2(num_pairs: int) -> List[str]:
     ) -> List[str]:
 
         if num_left_parens_needed > 0:  # i.e. able to append "(".
-            _directed_generate_balanced_parentheses(
+            _guided_generate_balanced_parentheses(
                 num_left_parens_needed - 1,
                 num_right_parens_needed,
                 valid_prefix + "(",
             )
 
         if num_left_parens_needed < num_right_parens_needed:  # i.e. able to append ')'.
-            _directed_generate_balanced_parentheses(
+            _guided_generate_balanced_parentheses(
                 num_left_parens_needed,
                 num_right_parens_needed - 1,
                 valid_prefix + ")",
@@ -75,14 +40,13 @@ def generate_balanced_parentheses_2(num_pairs: int) -> List[str]:
 
         return result
 
-    return _directed_generate_balanced_parentheses(
+    return _guided_generate_balanced_parentheses(
         num_pairs,
         num_pairs,
         "",
-        result=[],  # This _must_ be removed!
     )
 
 
 if __name__ == "__main__":
-    result = generate_balanced_parentheses_2(0)
+    result = generate_balanced_parentheses(0)
     print(result)
