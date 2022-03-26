98c27dca1cad43776043efaba04aa57f94413341
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-26 10:39:25 +0200

    clean up m_15_09_enumerate_trees.py

--- m_15_09_enumerate_trees.py
+++ m_15_09_enumerate_trees.py
@@ -4,67 +4,24 @@ from m_9_00_common import BinaryTreeNode
 
 
 def generate_all_binary_trees(num_nodes: int) -> List[Optional[BinaryTreeNode]]:
-    result: List[Optional[BinaryTreeNode]] = []  # can be deleted
-
-    # fmt: off
-    '''
-    def _helper(
-        # t: Optional[BinaryTreeNode],
-        size: int,
-        left: Optional[BinaryTreeNode] = None,
-        right: Optional[BinaryTreeNode] = None,
-    ) -> BinaryTreeNode:
-        if size == 0:
-            n = BinaryTreeNode(left=left, right=right)
-
-    _helper(None, num_nodes)
-    '''
-    # fmt: on
-
-    # fmt: off
-    '''
-    def _helper(
-        size: int,
-        size_left: int,
-    ) -> Optional[BinaryTreeNode]:
-        
-        if size_left == 0 and size == 1:
-            return BinaryTreeNode(left=None, right=None)
-        
-        left = _helper(
-            size_left,
-        )
-        right = 
-
-        return BinaryTreeNode()
-
-
-    for i in range(num_nodes):
-        _helper(size=num_nodes, size_left=i)
-    '''
-    # fmt: off
-
     def _helper(
         size: int,
     ) -> List[BinaryTreeNode]:
         if size == 0:
-            # return [BinaryTreeNode(left=None, right=None)]
             return [None]
-        
+
         trees: List[Optional[BinaryTreeNode]] = []
+
         for left_size in range(size):
             left_subtrees = _helper(left_size)
-            right_subtrees = _helper(size - left_size)  # change to `size - 1 - left_size`
+            right_subtrees = _helper(
+                size - 1 - left_size
+            )  # "-1" accounts for the root!
 
-        # return [
-        #     BinaryTreeNode(left=l, right=r)
-        #     for l in left_subtrees
-        #     for r in right_subtrees
-        # ]
             for l in left_subtrees:
                 for r in right_subtrees:
                     trees.append(BinaryTreeNode(left=l, right=r))
-        
+
         return trees
 
     return _helper(num_nodes)
@@ -91,7 +48,7 @@ def generate_all_binary_trees_2(num_nodes: int) -> List[Optional[BinaryTreeNode]
     return result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     result = generate_all_binary_trees(1)
 
     print(result)
