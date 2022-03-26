b976553fbabbc07080189d8d52a321a18f2be2e3
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-26 14:11:06 +0200

    clean up m_05_18_spiral_ordering.py

--- m_05_18_spiral_ordering.py
+++ m_05_18_spiral_ordering.py
@@ -2,6 +2,14 @@ from typing import List
 
 
 def s_1_matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
+    """
+    Return the "spiral ordering" of the `square_matrix`.
+
+    time: O(n^2)
+    """
+
+    spiral_ordering: List[int] = []
+
     def matrix_layer_in_clockwise(offset):
         if offset == len(square_matrix) - offset - 1:
             # `square_matrix` has odd dimension, and we are at its center.
@@ -17,7 +25,6 @@ def s_1_matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
             list(zip(*square_matrix))[offset][-1 - offset : offset : -1]
         )
 
-    spiral_ordering: List[int] = []
     for offset in range((len(square_matrix) + 1) // 2):
         matrix_layer_in_clockwise(offset)
 
@@ -25,7 +32,14 @@ def s_1_matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
 
 
 def s_1_matrix_in_spiral_order_refactored(square_matrix: List[List[int]]) -> List[int]:
+    """
+    Return the "spiral ordering" of the `square_matrix`.
+
+    time: O(n^2)
+    """
+
     spiral_ordering: List[int] = []
+
     for offset in range((len(square_matrix) + 1) // 2):
         if offset == len(square_matrix) - offset - 1:
             # `square_matrix` has odd dimension, and we are at its center.
@@ -45,6 +59,12 @@ def s_1_matrix_in_spiral_order_refactored(square_matrix: List[List[int]]) -> Lis
 
 
 def s_2_matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
+    """
+    Return the "spiral ordering" of the `square_matrix`.
+
+    time: O(n^2)
+    """
+
     value_absent_from_original_input = 0
     directions = ((0, 1), (1, 0), (0, -1), (-1, 0))
 
