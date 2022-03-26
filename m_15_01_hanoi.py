1c9e806b05bb22ea0aec59af64e46a50ac1ede67
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-03-26 10:22:06 +0200

    clean up m_15_01_hanoi.py

--- m_15_01_hanoi.py
+++ m_15_01_hanoi.py
@@ -2,8 +2,6 @@ from typing import List
 
 
 def compute_tower_hanoi(num_rings: int) -> List[List[int]]:
-    # result: List[List[int]] = []
-
     def _helper(
         n_rings: int,
         peg_0_idx: int,
@@ -27,11 +25,17 @@ def compute_tower_hanoi(num_rings: int) -> List[List[int]]:
 
 
 def compute_tower_hanoi_2(num_rings: int) -> List[List[int]]:
+    # fmt: off
+    # This commented-out block is actually unnecessary.
+    '''
     # Initialize pegs.
     NUM_PEGS = 3
+    # Here, a Python sequence is used to _emulate_ a Python dictionary.
     peg_2_rings: List[List[int]] = [
         list(reversed(range(1, num_rings + 1))),
     ] + [[] for _ in range(1, NUM_PEGS)]
+    '''
+    # fmt: on
 
     result: List[List[int]] = []
 
@@ -46,12 +50,19 @@ def compute_tower_hanoi_2(num_rings: int) -> List[List[int]]:
             _compute_tower_hanoi_steps(
                 n_rings - 1, source_peg, intermediary_peg, target_peg
             )
+
+            # fmt: off
+            # This commented-out block is actually unnecessary.
+            '''
             peg_2_rings[target_peg].append(
                 peg_2_rings[source_peg].pop(),
             )
+            '''
+            # fmt: on
             result.append(
                 [source_peg, target_peg],
             )
+
             _compute_tower_hanoi_steps(
                 n_rings - 1, intermediary_peg, target_peg, source_peg
             )
