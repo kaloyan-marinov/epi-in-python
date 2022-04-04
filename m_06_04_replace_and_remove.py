11dc29989e4706955e98b2658d64f522a8f7ec65
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-04-04 06:18:11 +0300

    add a docstring to m_06_04_replace_and_remove.py

--- m_06_04_replace_and_remove.py
+++ m_06_04_replace_and_remove.py
@@ -5,6 +5,20 @@ def replace_and_remove(
     size: int,
     s: List[str],
 ) -> int:
+    """
+    Perform an in-place modification of [the array] `s`,
+    which consists in applying the following 2 rules
+    to the first/leftmost `size`-many of `s`:
+
+        (a) replace each `a` with 2 `d`s
+
+        (b) remove each `b`
+
+    You do not need to worry about preserving subsequent entries.
+
+    You can assume that there is enough space in the array `s` to hold the final result.
+    """
+
     # Perform a forward iteration
     # to remove all `b`s and to count the number of `a`s.
     write_idx = 0
