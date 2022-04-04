ee26f921eabb614d4269cf847dd62a309220db3d
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-04-04 06:53:43 +0300

    add docstrings to and clean up m_07_06_delete_node_from_list.py

--- m_07_06_delete_node_from_list.py
+++ m_07_06_delete_node_from_list.py
@@ -1,9 +1,25 @@
-from m_7_00_common import ListNode
+"""
+Given a node in a singly-linked list,
+deleting it in O(1) time appears impossible
+b/c its predecessor's `next` field has to be updated.
+
+However, that can be achieved
+provided the following two conditions are satisfied:
+
+    (a) the node to delete is not the last one in the list, and
+
+    (b) it is easy to copy the value/`data` part of a node.
+"""
+
+from m_07_00_common import ListNode
 
 
 def deletion_from_list(node_to_delete: ListNode) -> None:
     """
     Assume `node_to_delete` is not the tail node.
+
+    time:  O(1)
+    space: O(1)
     """
     n = node_to_delete  # actually, can use `node_to_delete` directly instead of `n`
 
