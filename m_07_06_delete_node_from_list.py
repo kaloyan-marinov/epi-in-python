f27fd3d0edb48f321bb6f16fd3d5c78290e23c7f
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-04-04 06:54:53 +0300

    streamline m_07_06_delete_node_from_list.py

--- m_07_06_delete_node_from_list.py
+++ m_07_06_delete_node_from_list.py
@@ -21,10 +21,8 @@ def deletion_from_list(node_to_delete: ListNode) -> None:
     time:  O(1)
     space: O(1)
     """
-    n = node_to_delete  # actually, can use `node_to_delete` directly instead of `n`
+    # Copy the next node's data into the current node.
+    node_to_delete.data = node_to_delete.next.data
 
-    n.data = n.next.data
-
-    n_next_next = n.next.next
-    n.next.next = None  # actually, this can be commented out, b/c `n.next` represents the deleted node
-    n.next = n_next_next
+    # Delete the next node.
+    node_to_delete.next = node_to_delete.next.next
