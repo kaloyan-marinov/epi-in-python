2dc8968ad480d070ac6caa3f598fd36f54c8850d
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-04-04 06:32:03 +0300

    add docstrings to m_06_10_snake_string.py

--- m_06_10_snake_string.py
+++ m_06_10_snake_string.py
@@ -1,8 +1,40 @@
+"""
+We illustrate what it means to write a string in sinusoidal fashion
+by means of an example.
+The string "Hello World!" written in sinusoidal fashion is
+```
+ e    [blank]   l
+H  l o       W r d
+    l         o   !
+```
+
+Define "the snakestring of `s: str`" to be the left-to-right top-to-bottom sequence
+in which characters appear when `s` is written in sinusoidal fashion.
+For example, the snakestring of `"Hello World!" is "e lHloWrdlo!".
+"""
+
+
 def snake_string_1(s: str) -> str:
+    """
+    Return the snakestring of `s`.
+
+    time:  O(n)
+           n := len(s)
+
+    space: O(n)
+    """
     return s[1::4] + s[::2] + s[3::4]
 
 
 def snake_string_2(s: str) -> str:
+    """
+    Return the snakestring of `s`.
+
+    time:  O(n)
+           n := len(s)
+
+    space: O(n)
+    """
     result = []
     for i in range(1, len(s), 4):
         result.append(s[i])
