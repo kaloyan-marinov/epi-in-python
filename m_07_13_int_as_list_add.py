1e1674711c5996c96f4ff9bc8a0aafde01b47f64
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-04-04 07:44:27 +0300

    clean up m_07_13_int_as_list_add.py

--- m_07_13_int_as_list_add.py
+++ m_07_13_int_as_list_add.py
@@ -1,9 +1,30 @@
 from typing import Optional
 
-from m_7_00_common import ListNode
+from m_07_00_A_singly_linked_list import ListNode
 
 
-def add_two_numbers(L1: ListNode, L2: ListNode) -> Optional[ListNode]:
+def add_two_numbers(L1: ListNode, L2: ListNode) -> ListNode:
+    """
+    (
+    This is my solution.
+    While it works, the official solution is arguably more elegant.
+    )
+
+    Assume that each of `L1` and `L2` represents an unbounded integer, i.e.
+        (a) the `data` field of each node stores a [decimal] digit,
+        (b) the least significant digit is stored in the head node, and
+        (c) the `data` field of the tail node is non-zero.
+
+    Return the head of a linked list,
+    which corresponds to the sum of the integers represented by `L1` and `L2`.
+
+    time:  O(n + m)
+           n := the # of nodes in `L1`
+           m := the # of nodes in `L2`
+
+    space: O(max(n, m))
+    """
+
     dummy_head = ListNode(data=0, next=None)
 
     L = dummy_head
@@ -29,36 +50,42 @@ def add_two_numbers(L1: ListNode, L2: ListNode) -> Optional[ListNode]:
         L_next = ListNode(data=L.data // 10, next=None)
         L.data %= 10
         L.next = L_next
-        # L = L_next  # can be commented out
 
     return dummy_head.next
 
 
 def add_two_numbers_2(L1: ListNode, L2: ListNode) -> Optional[ListNode]:
+    """
+    (This is the official solution.)
+
+    Assume that each of `L1` and `L2` represents an unbounded integer, i.e.
+        (a) the `data` field of each node stores a [decimal] digit,
+        (b) the least significant digit is stored in the head node, and
+        (c) the `data` field of the tail node is non-zero.
+
+    Return the head of a linked list,
+    which corresponds to the sum of the integers represented by `L1` and `L2`.
+
+    time:  O(n + m)
+           n := the # of nodes in `L1`
+           m := the # of nodes in `L2`
+
+    space: O(max(n, m))
+    """
+
     dummy_head = ListNode(data=0, next=None)
 
     L = dummy_head
-    while L1 or L2:
-        s = L.data // 10 + (L1.data if L1 else 0) + (L2.data if L2 else 0)
+    carry = 0
+    while L1 or L2 or carry > 0:
+        s = carry + (L1.data if L1 else 0) + (L2.data if L2 else 0)
 
-        L.data %= 10
-        L_next = ListNode(data=s, next=None)
-        L.next = L_next
+        L.next = ListNode(data=s % 10)
 
         L = L.next
-        if L1:
-            L1 = L1.next
-        if L2:
-            L2 = L2.next
-
-    if L.data > 9:
-        s = L.data // 10
-
-        L.data %= 10
-        L_next = ListNode(data=s, next=None)
-        L.next = L_next
-
-        # L = L.next  # can be commented out
+        L1 = L1.next if L1 else None
+        L2 = L2.next if L2 else None
+        carry = s // 10
 
     return dummy_head.next
 
