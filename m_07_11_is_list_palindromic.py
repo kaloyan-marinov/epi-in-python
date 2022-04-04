d031ca64dae001243260a93fb90126c4598be6c4
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-04-04 07:40:23 +0300

    clean up m_07_11_is_list_palindromic.py

--- m_07_11_is_list_palindromic.py
+++ m_07_11_is_list_palindromic.py
@@ -1,43 +1,39 @@
-from m_7_00_common import ListNode, length, reverse_list
+from m_07_00_A_singly_linked_list import ListNode
+from m_07_00_D_reverse_linked_list import reverse_list
 
 
 def is_linked_list_a_palindrome_1(L: ListNode) -> bool:
     """
-    strategy/approach:
+    (This is my own solution.)
+
+    Determine whether the linked list starting at `L` is palindromic.
+
+    (
+    This solution is based on the following strategy/approach:
         - compare the 1st and last nodes
         - compare the 2nd and 2nd-to-last nodes
         - etc.
+    where the linked list is repeatedly traversed to identify
+    its last node; its second-to-last node; and so on.
+    )
 
-    time: O(n^2),
-          n := the # of nodes in L
+    time:  O(n^2),
+           n := the # of nodes in L
 
-    ---
-
-    This function passes all 203 of the test cases within the EPIJudge,
-    but the very last one takes significantly longer than the others to run.
+    space: O(1)
     """
-    n = length(L)
-
-    right_it = L
-    for _ in range(n // 2 + n % 2):
-        right_it = right_it.next
-
-    for shift_for_left_it in reversed(range(n // 2)):
-        left_it = L
-        for _ in range(shift_for_left_it):
-            left_it = left_it.next
-
-        if left_it.data != right_it.data:
-            return False
-
-        right_it = right_it.next
-
-    return True
+    # To find an implementation of this solution, check the history of this file.
+    pass
 
 
 def is_linked_list_a_palindrome_2(L: ListNode) -> bool:
     """
-    strategy/approach:
+    (This is the official solution.)
+
+    Determine whether the linked list starting at `L` is palindromic.
+
+    (
+    This solution is based on the following strategy/approach:
         - pay a 1-time cost of O(n) time complexity
           to obtain the reverse of the 2nd half of the original list
         - test for palindromicity
@@ -46,12 +42,15 @@ def is_linked_list_a_palindrome_2(L: ListNode) -> bool:
     NB: this strategy/approach does change the original list,
         but the original list can be restored by reversing the already-reversed sublist
         (as soon as the palindromicity test has been performed)
+    )
 
-    time:  O(n)
+    time:  O(n^2),
+           n := the # of nodes in L
+
     space: O(1)
     """
 
-    # Find the second half of L.
+    # Compute the middle node of `L`.
     slow = L
     fast = L
     while fast and fast.next:
