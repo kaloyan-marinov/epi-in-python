76cfafc85c137225050cda6ca6db342c4d56ec76
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-04-04 06:19:01 +0300

    clean up m_06_04_replace_and_remove.py

--- m_06_04_replace_and_remove.py
+++ m_06_04_replace_and_remove.py
@@ -6,6 +6,8 @@ def replace_and_remove(
     s: List[str],
 ) -> int:
     """
+    Assume that each entry in `s` is a single character.
+
     Perform an in-place modification of [the array] `s`,
     which consists in applying the following 2 rules
     to the first/leftmost `size`-many of `s`:
@@ -14,19 +16,22 @@ def replace_and_remove(
 
         (b) remove each `b`
 
-    You do not need to worry about preserving subsequent entries.
-
-    You can assume that there is enough space in the array `s` to hold the final result.
+    This implementation:
+        - does not need to worry about preserving subsequent entries;
+        - assumes that there is enough space in the array `s` to hold the final result.
     """
 
     # Perform a forward iteration
     # to remove all `b`s and to count the number of `a`s.
-    write_idx = 0
     a_count = 0
+    write_idx = 0
+
     for i in range(size):
-        if s[i] != "b":
-            s[write_idx] = s[i]
-            write_idx += 1
+        if s[i] == "b":
+            continue
+
+        s[write_idx] = s[i]
+        write_idx += 1
 
         if s[i] == "a":
             a_count += 1
@@ -34,9 +39,10 @@ def replace_and_remove(
     # Perform a backward iteration:
     # replace each `a` with `dd`
     # (starting from the [right] end of the "end result").
+    final_size = write_idx + a_count
+
     cur_idx = write_idx - 1
-    write_idx += a_count - 1
-    final_size = write_idx + 1
+    write_idx = final_size - 1
 
     while cur_idx >= 0:
         if s[cur_idx] == "a":
