c9a3e63df41eba8ee4c34b0126e5b2d18e99e351
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-04-04 06:06:42 +0300

    add another solution and docstrings to m_06_02_convert_base.py

--- m_06_02_convert_base.py
+++ m_06_02_convert_base.py
@@ -13,16 +13,32 @@ CHAR_2_DIGIT.update(
         "F": 15,
     }
 )
+# fmt: off
+# Since `string.hexdigits` equals '0123456789abcdefABCDEF',
+# the previous block is equivalent to the following one:
+'''
+CHAR_2_DIGIT = {c.upper(): string.digits.index(c) for c in string.digits[:16]}
+'''
+# fmt: on
 
 DIGIT_2_CHAR = {v: k for k, v in CHAR_2_DIGIT.items()}
 
 
 def string_to_int(s: str, b: int) -> int:
+    """
+    Assume that:
+        (a) `2 <= b <= 16`
+        (b) `s` represents a integer in base `b`
+        (c) `s` is either '-' or a base-`b` digit
+
+    Return the integer's representation in base 10.
+    """
+
     sign = -1 if s[0] == "-" else 1
 
     absolute_value = functools.reduce(
         lambda running_sum, c: b * running_sum + CHAR_2_DIGIT[c],
-        s[s[0] in "-+" :],
+        s[s[0] in "-" :],
         0,
     )
 
@@ -30,21 +46,17 @@ def string_to_int(s: str, b: int) -> int:
 
 
 def int_to_string(x: int, b: int) -> str:
+    """
+    Assume that `2 <= b <= 16`.
+
+    Return the representation of `x` in base `b`.
+    """
+
     is_negative = False
     if x < 0:
         x = -x
         is_negative = True
 
-    # fmt: off
-    # the following breaks down when x is 0
-    '''
-    digits = []
-    while x:
-        d = x % b
-        digits.append(DIGIT_2_CHAR[d])
-        x //= b
-    '''
-    # fmt: on
     if x == 0:
         digits = ["0"]
     else:
@@ -57,14 +69,43 @@ def int_to_string(x: int, b: int) -> str:
     return ("-" if is_negative else "") + "".join(reversed(digits))
 
 
-def int_to_string_recursively(x: int, b: int) -> str:
-    return "" if x == 0 else int_to_string(x // b, b) + string.hexdigits[x % b].upper()
-
-
 def convert_base(
     num_as_str: str,
     b1: int,
     b2: int,
 ) -> str:
+    """
+    Assume that:
+        (a) `2 <= b1, b2 <= 16`
+        (b) `num_as_str` represents an integer in base `b1`
+
+    Return a string representing the integer in base `b2`
+    """
     x = string_to_int(num_as_str, b1)
     return int_to_string(x, b2)
+
+
+def convert_base_2(
+    num_as_str: str,
+    b1: int,
+    b2: int,
+) -> str:
+
+    is_negative = False
+    if num_as_str[0] == "-":
+        is_negative = True
+        num_as_str = num_as_str[1:]
+
+    x = string_to_int(num_as_str, b1)
+
+    return ("-" if is_negative else "") + (
+        "0" if x == 0 else int_to_string_recursively(x, b2)
+    )
+
+
+def int_to_string_recursively(x: int, b: int) -> str:
+    return (
+        ""
+        if x == 0
+        else int_to_string_recursively(x // b, b) + string.hexdigits[x % b].upper()
+    )
