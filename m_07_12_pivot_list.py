7ff48e503fd94eb5f0d2daf39230a5cda8c08153
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-04-04 07:42:19 +0300

    clean up m_07_12_pivot_list.py

--- m_07_12_pivot_list.py
+++ m_07_12_pivot_list.py
@@ -1,34 +1,51 @@
 from typing import Optional
 
-from m_7_00_common import ListNode
+from m_07_00_A_singly_linked_list import ListNode
 
 
-def list_pivoting(L: ListNode, k: int) -> Optional[ListNode]:
-    l = ListNode(data=0, next=None)
-    latest_l = l
-    e = ListNode(data=0, next=None)
-    latest_e = e
-    g = ListNode(data=0, next=None)
-    latest_g = g
+def list_pivoting(L: Optional[ListNode], k: int) -> Optional[ListNode]:
+    """
+    Perform an in-place modification of the linked list starting at `L`,
+    consisting in a re-ordering of its nodes s.t.
+    all nodes containing keys `< k` appear before all nodes containing `k`
+    and all nodes containing `k` appear before all nodes containing keys `> k`,
+    and return the head node of the resulting linked list.
+
+    The relative ordering of the nodes with keys `< k` remain unchanges;
+    the same holds true for the nodes with keys `= k`;
+    the same holds true for the nodes with keys `> k`.
+
+    time:  O(n)
+           n := the # of nodes in `L`
+
+    space: O(1)
+    """
+
+    l_dummy_head = ListNode(data=0, next=None)
+    l_tail = l_dummy_head
+    e_dummy_head = ListNode(data=0, next=None)
+    e_tail = e_dummy_head
+    g_dummy_head = ListNode(data=0, next=None)
+    g_tail = g_dummy_head
 
     while L:
         L_next = L.next
 
-        L.next = None
+        L.next = None  # NB: This can be commented out, and the f-n will still work!
         if L.data < k:
-            latest_l.next = L
-            latest_l = latest_l.next
+            l_tail.next = L
+            l_tail = l_tail.next
         elif L.data == k:
-            latest_e.next = L
-            latest_e = latest_e.next
+            e_tail.next = L
+            e_tail = e_tail.next
         else:
-            latest_g.next = L
-            latest_g = latest_g.next
+            g_tail.next = L
+            g_tail = g_tail.next
 
         L = L_next
 
-    latest_g.next = None
-    latest_e.next = g.next
-    latest_l.next = e.next
+    g_tail.next = None
+    e_tail.next = g_dummy_head.next
+    l_tail.next = e_dummy_head.next
 
-    return l.next
+    return l_dummy_head.next
