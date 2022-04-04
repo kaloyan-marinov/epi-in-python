a02079b443846619dc7aa5fbd6b6f4218c1ecf8b
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-04-04 06:20:29 +0300

    add docstrings to m_06_05_is_string_palindromic_punctuation.py

--- m_06_05_is_string_palindromic_punctuation.py
+++ m_06_05_is_string_palindromic_punctuation.py
@@ -1,4 +1,16 @@
 def is_palindrome(s: str) -> bool:
+    """
+    Determine whether `s`,
+    when all its non-alphanumeric characters are removed
+    and character case is ignored,
+    reads the same back-to-front.
+
+    time:  O(n)
+           where n := len(s)
+
+    space: O(1)
+    """
+
     i = 0
     j = len(s) - 1
 
@@ -13,14 +25,25 @@ def is_palindrome(s: str) -> bool:
 
         if s[i].lower() != s[j].lower():
             return False
-        else:
-            i += 1
-            j -= 1
+
+        i += 1
+        j -= 1
 
     return True
 
 
 def is_palindrome_pythonic(s: str) -> bool:
+    """
+    Determine whether `s`,
+    when all its non-alphanumeric characters are removed
+    and character case is ignored,
+    reads the same back-to-front.
+
+    time:  O(n)
+           where n := len(s)
+
+    space: O(1)
+    """
     return all(
         a == b
         for a, b in zip(
