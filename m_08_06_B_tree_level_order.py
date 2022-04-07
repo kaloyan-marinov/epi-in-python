796e70ca040e591058e7edda415990078881574a
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2022-04-07 19:49:52 +0300

    add a docstring to m_08_06_B_tree_level_order.py

--- m_08_06_B_tree_level_order.py
+++ m_08_06_B_tree_level_order.py
@@ -1,9 +1,44 @@
-from typing import List
+from typing import List, Optional
 
 from m_9_00_common import BinaryTreeNode
 
 
-def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
+def binary_tree_depth_order(tree: Optional[BinaryTreeNode]) -> List[List[int]]:
+    """
+    Return a list of [sub]lists,
+    where each sublist consists of the keys at a fixed depth in `tree`
+    and the sublists should appear in the order of increasing depths.
+
+    For example, for the binary tree
+
+                                314
+                    /                       \
+            6                                       6
+          /    \                                  /      \
+        271     561                             2           271
+       /   \       \                              \            \
+    28      0       3                               1           28
+                  /                                /  \
+                17                              401     257
+                                                   \
+                                                    641
+    the return value should be
+
+    [
+        [314],
+        [6, 6],
+        [271, 561, 2, 271],
+        [28, 0, 3, 1, 28],
+        [17, 401, 257],
+        [641],
+    ]
+
+    time:  O(n)
+           where n := the # of nodes in the `tree`
+
+    space: O(m)
+           where m := the max # of nodes at any given depth in the `tree`
+    """
 
     result: List[List[int]] = []
     if not tree:
